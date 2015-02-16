04d02bbef9b10f607fe8d3abd0286fed48035b47
jonathanvdc <jonathan.vdc@outlook.com>
2015-02-16 22:51:30 +0100

    Removed outdated "Test" folder in dsc/bin/Release
    These "tests" were mainly copies of various D# Flame projects which were
    placed there before dsc could compile itself. Other tests were projects
    which should not be qualified as tests.

--- dsc/bin/Release/Test/CppPluggedTest/bin/StackTest.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include <memory>
-#include "Stack.h"
-#include "StackTest.h"
-
-StackTest::StackTest()
-{
-    this->x = std::shared_ptr<Stack<int>>(new Stack<int>());
-}
--- dsc/bin/Release/Test/GADS2/bin/IRecord.py
+++
@@ -1,7 +0,0 @@
-class IRecord:
-    """ Describes a generic record. """
-
-    @property
-    def key(self):
-        """ Gets the record's search key. """
-        raise NotImplementedError("Getter of property 'IRecord.key' was not implemented.")
