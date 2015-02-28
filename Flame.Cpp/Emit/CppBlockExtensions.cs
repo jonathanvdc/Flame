9e92094eb73e0c264ea4418a63d16441c4acb334
jonathanvdc <jonathan.vdc@outlook.com>
2015-02-28 18:34:16 +0100

    for loop support in code generators
    All code generators that inherit from IForCodeGenerator will now
    generate for loops when a ForStatement is emitted (i.e., a for loop is
    directly or indirectly generated). The C++ back-end supports this
    functionality. Code generators that do not implement IForCodeGenerator
    will emulate for loops with a traditional while loop construction.

---
+++ Flame.Cpp/Emit/CppBlockExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flame.Cpp.Emit
+{
+    public static class CppBlockExtensions
+    {
+        #region IsSimple
+
+        /// <summary>
+        /// Gets a boolean value that identifies the block as either a simple or complex statement.
+        /// </summary>
+        /// <param name="Block"></param>
+        /// <returns></returns>
+        public static bool IsSimple(this ICppBlock Block)
+        {
+            if (Block is CppBlockGeneratorBase)
+            {
+                return Block is CppBlockGenerator && ((CppBlockGenerator)Block).StatementCount <= 1;
+            }
+            return true;
+        }
+
+        #endregion
+
+        #region GetDeclarations
+
+        public static IEnumerable<LocalDeclaration> GetLocalDeclarations(this ICppBlock Block)
+        {
+            if (Block is LocalDeclarationReference)
+            {
+                return new LocalDeclaration[] { ((LocalDeclarationReference)Block).Declaration };
+            }
+            else if (Block is ICppLocalDeclaringBlock)
+            {
+                return ((ICppLocalDeclaringBlock)Block).LocalDeclarations;
+            }
+            else
+            {
+                return Enumerable.Empty<LocalDeclaration>();
+            }
+        }
+
+        public static IEnumerable<LocalDeclaration> GetLocalDeclarations(this IEnumerable<ICppBlock> Blocks)
+        {
+            return Blocks.SelectMany((item) => item.GetLocalDeclarations());
+        }
+
+        #endregion
+
+        #region DeclaresLocal
+
+        public static bool DeclaresLocal(this ICppBlock Block, CppLocal Local)
+        {
+            return Block.GetLocalDeclarations().Any(item => item.Local == Local);
+        }
+
+        #endregion
+    }
+}
