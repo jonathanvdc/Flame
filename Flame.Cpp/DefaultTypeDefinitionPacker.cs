4addb15ae87b4cb82886e50f5ea63a45d1823741
jonathanvdc <jonathan.vdc@outlook.com>
2015-03-22 16:15:17 +0100

    Changed C++ default type definition packer
    Operators and casts are now packed separately

--- Flame.Cpp/DefaultTypeDefinitionPacker.cs
+++ Flame.Cpp/DefaultTypeDefinitionPacker.cs
@@ -31,13 +31,24 @@ namespace Flame.Cpp
             IEnumerable<ICppMember> ctors = Members.OfType<IMethod>().Where(item => item.IsConstructor).Cast<ICppMember>();
             IEnumerable<ICppMember> fields = Members.OfType<IField>().Cast<ICppMember>();
             IEnumerable<IProperty> props = Members.OfType<IProperty>();
-            IEnumerable<ICppMember> methods = Members.OfType<IMethod>().Where(item => !item.IsConstructor).Cast<ICppMember>();
+            IEnumerable<ICppMember> methods = Members.OfType<IMethod>().Where(item => !item.IsConstructor && !item.get_IsOperator() && !item.get_IsCast()).Cast<ICppMember>();
+            IEnumerable<ICppMember> operators = Members.OfType<IMethod>().Where(item => !item.IsConstructor && item.get_IsOperator() && !item.get_IsCast()).Cast<ICppMember>();
+            IEnumerable<ICppMember> casts = Members.OfType<IMethod>().Where(item => !item.IsConstructor && !item.get_IsOperator() && item.get_IsCast()).Cast<ICppMember>();
+            
+            Func<ICppMember, CppMemberPack> packSingle = item => new CppMemberPack(item);
+            Func<IEnumerable<ICppMember>, IEnumerable<CppMemberPack>> packIndividually = items => items.Select(packSingle);
 
             var accessorPacks = props.Select(item => new CppMemberPack(item.GetAccessors().OfType<ICppMember>()));
-            var methodPacks = methods.Select(item => new CppMemberPack(item));
-            var typePacks = Members.OfType<IType>().Cast<ICppMember>().Select(item => new CppMemberPack(item));
+            var methodPacks = packIndividually(methods);
+            var opPacks = packIndividually(operators.Concat(casts));
+            var typePacks = packIndividually(Members.OfType<IType>().Cast<ICppMember>());
 
-            return new CppMemberPack[] { new CppMemberPack(ctors) }.Concat(methodPacks).Concat(accessorPacks).With(new CppMemberPack(fields)).Concat(typePacks);
+            return new CppMemberPack[] { new CppMemberPack(ctors) }
+                .Concat(typePacks)
+                .Concat(methodPacks)
+                .Concat(accessorPacks)
+                .Concat(opPacks)
+                .With(new CppMemberPack(fields));
         }
     }
 }
