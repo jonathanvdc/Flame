23758f562850214a31e0ef4b56e23e81ef670fe3
jonathanvdc <jonathan.vdc@outlook.com>
2015-03-29 15:50:16 +0200

    More Flame.Front types

---
+++ Flame.Front/EmptyAssemblyResolver.cs
@@ -0,0 +1,21 @@
+﻿using Flame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flame.Front
+{
+    public class EmptyAssemblyResolver : IAssemblyResolver
+    {
+        public async Task<IAssembly> ResolveAsync(string Identifier, IDependencyBuilder DependencyBuilder)
+        {
+            return null;
+        }
+
+        public async Task CopyAsync(string SourceIdentifier, string TargetIdentifier)
+        {
+        }
+    }
+}
