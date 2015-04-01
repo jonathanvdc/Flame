1343c4cf5cf7e1506445d61a1a42c67e70b1cc75
jonathanvdc <jonathan.vdc@outlook.com>
2015-04-01 16:50:20 +0200

    Added method diffs to D# front-end and Flame.Front

---
+++ Flame.Front/Cli/HighlightingNodeWriter.cs
@@ -0,0 +1,50 @@
+﻿using Pixie;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flame.Front.Cli
+{
+    public class HighlightingNodeWriter : INodeWriter
+    {
+        public HighlightingNodeWriter(IConsole Console, INodeWriter MainWriter, Style HighlightStyle, Style HighlightMissingStyle, Style HighlightExtraStyle)
+        {
+            this.Console = Console;
+            this.MainWriter = MainWriter;
+            this.HighlightStyle = HighlightStyle;
+            this.HighlightMissingStyle = HighlightMissingStyle;
+            this.HighlightExtraStyle = HighlightExtraStyle;
+        }
+
+        public INodeWriter MainWriter { get; private set; }
+        public IConsole Console { get; private set; }
+        public Style HighlightStyle { get; private set; }
+        public Style HighlightMissingStyle { get; private set; }
+        public Style HighlightExtraStyle { get; private set; }
+
+        public void Write(IMarkupNode Node)
+        {
+            string type = Node.Attributes.Get<string>("type", "default");
+            if ("missing".Equals(type, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.PushStyle(HighlightMissingStyle);
+            }
+            else if ("extra".Equals(type, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.PushStyle(HighlightExtraStyle);
+            }
+            else
+            {
+                Console.PushStyle(HighlightStyle);
+            }
+            Console.Write(Node.GetText());
+            foreach (var item in Node.Children)
+            {
+                MainWriter.Write(item);
+            }
+            Console.PopStyle();
+        }
+    }
+}
---
+++ Flame.Front/Cli/RemarksNodeWriter.cs
@@ -0,0 +1,36 @@
+﻿using Pixie;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flame.Front.Cli
+{
+    public class RemarksNodeWriter : INodeWriter
+    {
+        public RemarksNodeWriter(IConsole Console, INodeWriter MainWriter, Style RemarksStyle)
+        {
+            this.Console = Console;
+            this.MainWriter = MainWriter;
+            this.RemarksStyle = RemarksStyle;
+        }
+
+        public INodeWriter MainWriter { get; private set; }
+        public IConsole Console { get; private set; }
+        public Style RemarksStyle { get; private set; }
+
+        public void Write(IMarkupNode Node)
+        {
+            Console.WriteLine();
+            Console.PushStyle(RemarksStyle);
+            Console.Write("Remarks: ");
+            Console.Write(Node.GetText());
+            foreach (var item in Node.Children)
+            {
+                MainWriter.Write(item);
+            }
+            Console.PopStyle();
+        }
+    }
+}
