6c138737b82d88e2cd4d9ff6d1c99a1ae541af74
jonathanvdc <jonathan.vdc@outlook.com>
2015-04-02 19:35:15 +0200

    Eliminated wacky whiteline writing in ConsoleLog
    WriteGap()/WriteWhiteline() have been replaced by ParagraphConsole,
    which does a far better job at formatting output.

---
+++ Flame.Front/Cli/ParagraphConsole.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flame.Front.Cli
+{
+    public class ParagraphConsole : IConsole
+    {
+        public ParagraphConsole(IConsole Console)
+        {
+            this.Console = Console;
+        }
+
+        public IConsole Console { get; private set; }
+
+        private int newlineCount;
+
+        public ConsoleDescription Description
+        {
+            get { return Console.Description; }
+        }
+
+        public void PushStyle(Style Value)
+        {
+            Console.PushStyle(Value);
+        }
+
+        public void PopStyle()
+        {
+            Console.PopStyle();
+        }
+
+        public void Write(string Text)
+        {
+            lock (Console)
+            {
+                Console.Write(Text);
+                newlineCount = 0;
+            }
+        }
+
+        public void WriteLine()
+        {
+            lock (Console)
+            {
+                Console.WriteLine();
+                newlineCount++;
+            }
+        }
+
+        public void WriteSeparator(int Size)
+        {
+            lock (Console)
+            {
+                if (Size > newlineCount)
+                {
+                    while (newlineCount < Size)
+                    {
+                        Console.WriteLine();
+                        newlineCount++;
+                    }
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            Console.Dispose();
+        }
+    }
+}
