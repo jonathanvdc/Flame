802bb060ead7b5c33d33da4ef9b566c8fd31de4d
jonathanvdc <jonathan.vdc@outlook.com>
2015-04-24 19:20:37 +0200

    Changed default documentation formatting to blocks
    BlockDocumentationFormatter is now used by default. It formats lines
    based on whitespace, punctuation, a max length and a suggested length.
    Its results are usually aesthetically pleasing.

---
+++ Flame.CodeDescription/BlockDocumentationFormatter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flame.CodeDescription
+{
+    public sealed class BlockDocumentationFormatter : IDocumentationFormatter
+    {
+        public BlockDocumentationFormatter(int SuggestedBlockWidth, int MaxBlockWidth)
+        {
+            this.SuggestedBlockWidth = SuggestedBlockWidth;
+            this.MaxBlockWidth = MaxBlockWidth;
+        }
+
+        public int SuggestedBlockWidth { get; private set; }
+        public int MaxBlockWidth { get; private set; }
+
+        public static BlockDocumentationFormatter Default
+        {
+            get
+            {
+                return new BlockDocumentationFormatter(DefaultSuggestedBlockWidth, DefaultMaxBlockWidth);
+            }
+        }
+
+        public const int DefaultSuggestedBlockWidth = 30;
+        public const int DefaultMaxBlockWidth = 60;
+
+        public string Format(IEnumerable<DescriptionAttribute> Attributes)
+        {
+            StringBuilder sb = new StringBuilder();
+            var summaryAttr = Attributes.WithTag("summary");
+            if (summaryAttr != null)
+            {
+                sb.AppendLine(DocumentationExtensions.IntroduceBlockLineBreaks(summaryAttr.Description, SuggestedBlockWidth, MaxBlockWidth));
+            }
+            foreach (var item in Attributes.ExcludeTag("summary"))
+            {
+                sb.AppendLine(DocumentationExtensions.ChangeFirstCharacter(item.Tag.ToLower(), char.ToUpper) + ":");
+                sb.AppendLine(DocumentationExtensions.IntroduceBlockLineBreaks(item.Description, SuggestedBlockWidth, MaxBlockWidth));
+            }
+            return sb.ToString();
+        }
+    }
+}
