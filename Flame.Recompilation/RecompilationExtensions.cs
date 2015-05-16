3b51565106e2f59127cd9efd87e1e7167301c910
jonathanvdc <jonathan.vdc@outlook.com>
2015-05-16 14:51:05 +0200

    Build target parsers can now create pass suites

---
+++ Flame.Recompilation/RecompilationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Flame.Compiler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flame.Recompilation
+{
+    public static class RecompilationExtensions
+    {
+        public static IMethodOptimizer GetMethodOptimizer(this ICompilerLog Log)
+        {
+            return Log.Options.GetOption<IMethodOptimizer>("optimize", null) ?? new DefaultOptimizer();
+        }
+    }
+}
