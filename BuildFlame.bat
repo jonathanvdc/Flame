05fc9bdd069b09e8e492ff032fa77830cb9670e2
jonathanvdc <jonathan.vdc@outlook.com>
2015-05-17 19:44:10 +0200

    Added "predicate nodes" (IPredicateNode)
    These nodes provide a fast path for predicate visitors.

---
+++ BuildFlame.bat
@@ -0,0 +1,4 @@
+dsc Flame\Flame.dsc.dsproj -pedantic -time
+dsc Flame.Compiler\Flame.Compiler.dsc.dsproj -pedantic -time
+dsc Flame.Syntax\Flame.Syntax.dsc.dsproj -pedantic -time
+dsc Flame.DSharp\Flame.DSharp.dsc.dsproj -pedantic -time
