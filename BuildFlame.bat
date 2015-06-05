d55425a8f64767409d9da23fa8233c8012a75e2e
jonathanvdc <jonathan.vdc@outlook.com>
2015-06-05 18:20:30 +0200

    Added Flame.Optimization
    Flame.Optimization contains an inlining pass right now, but could
    contain some other passes in the future as well.

--- BuildFlame.bat
+++ BuildFlame.bat
@@ -1,4 +1,5 @@
 dsc Flame\Flame.dsc.dsproj -pedantic -time
 dsc Flame.Compiler\Flame.Compiler.dsc.dsproj -pedantic -time
+dsc Flame.Optimization\Flame.Optimization.dsproj -pedantic -time
 dsc Flame.Syntax\Flame.Syntax.dsc.dsproj -pedantic -time
 dsc Flame.DSharp\Flame.DSharp.dsc.dsproj -pedantic -time
