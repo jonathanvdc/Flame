df391a08d3b618a128bcd9471e1864de58a11ce8
jonathanvdc <jonathan.vdc@outlook.com>
2015-07-24 15:18:17 +0200

    Removed while/do...while loops from Flame.MIPS

---
+++ Flame.MIPS/Emit/BranchFlowStructure.cs
@@ -0,0 +1,35 @@
+﻿using Flame.Compiler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flame.MIPS.Emit
+{
+    public class BranchFlowStructure : IFlowControlStructure
+    {
+        public BranchFlowStructure(ICodeGenerator CodeGenerator, BlockTag Tag, ILabel Start, ILabel End)
+        {
+            this.CodeGenerator = CodeGenerator;
+            this.Tag = Tag;
+            this.Start = Start;
+            this.End = End;
+        }
+
+        public ICodeGenerator CodeGenerator { get; private set; }
+        public BlockTag Tag { get; private set; }
+        public ILabel Start { get; private set; }
+        public ILabel End { get; private set; }
+
+        public IAssemblerBlock EmitBreak()
+        {
+            return (IAssemblerBlock)End.EmitBranch(CodeGenerator.EmitBoolean(true));
+        }
+
+        public IAssemblerBlock EmitContinue()
+        {
+            return (IAssemblerBlock)Start.EmitBranch(CodeGenerator.EmitBoolean(true));
+        }
+    }
+}
