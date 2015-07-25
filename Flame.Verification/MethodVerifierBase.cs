2ea492ebf538fcac76ecb5e913c30d02963aa589
jonathanvdc <jonathan.vdc@outlook.com>
2015-07-25 13:55:24 +0200

    Added an access modifier verification pass, fixed some bugs

--- Flame.Verification/MethodVerifierBase.cs
+++ Flame.Verification/MethodVerifierBase.cs
@@ -87,7 +87,7 @@ namespace Flame.Verification
                 if (item.ParameterType == null)
                 {
                     Log.LogError(new LogEntry("Invalid (unresolved?) parameter type",
-                        "Parameter '" + item.Name + "' of method '" + Member.FullName + "' has a null parameter type.",
+                        "Parameter '" + item.Name + "' of " + SingularMemberKindName + " '" + Member.FullName + "' has a null parameter type.",
                         Member.GetSourceLocation()));
                     return false;
                 }
@@ -99,7 +99,17 @@ namespace Flame.Verification
                 if (!item.get_IsVirtual() && !item.get_IsAbstract() && !item.DeclaringType.get_IsInterface())
                 {
                     Log.LogError(new LogEntry("Invalid base method",
-                        "Method '" + Member.Name + "' of '" + Member.DeclaringType + "' has a non-virtual, non-abstract and non-interface base method.",
+                        GetDescription(Member) + " has a non-virtual, non-abstract and non-interface base method, declared in '" + item.DeclaringType.FullName + "'",
+                        Member.GetSourceLocation()));
+                    success = false;
+                }
+
+                if (item.get_Access() != Member.get_Access())
+                {
+                    Log.LogError(new LogEntry("Access modifier mismatch",
+                        GetDescription(Member) + " was marked '" + Member.get_Access().GetAccessModifierName() +
+                        "' but its base method in '" + item.DeclaringType.FullName + "' was declared '" + item.get_Access().GetAccessModifierName() + "'. " + 
+                        "Access modifiers should remain the same within a method inheritance tree.",
                         Member.GetSourceLocation()));
                     success = false;
                 }
@@ -111,7 +121,7 @@ namespace Flame.Verification
             if (Member.get_IsAbstract() && !Member.DeclaringType.get_IsAbstract() && !Member.DeclaringType.get_IsInterface())
             {
                 Log.LogError(new LogEntry("Abstract method in non-abstract type",
-                    "Method '" + Member.Name + "' of '" + Member.DeclaringType + "' has been marked abstract, but its declaring type is neither abstract nor an interface.",
+                    GetDescription(Member) + " has been marked abstract, but its declaring type is neither abstract nor an interface.",
                     Member.GetSourceLocation()));
                 success = false;
             }
