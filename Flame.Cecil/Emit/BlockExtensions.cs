679c63e6452c004773c27625e31d3811e497625a
jonathanvdc <jonathan.vdc@outlook.com>
2015-07-26 23:10:40 +0200

    Flame.Cecil now produces better code for `if` statements with empty blocks

---
+++ Flame.Cecil/Emit/BlockExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flame.Cecil.Emit
+{
+    public static class BlockExtensions
+    {
+        /// <summary>
+        /// Tries to determine conservatively if this block is empty.
+        /// </summary>
+        /// <param name="Block"></param>
+        /// <returns></returns>
+        public static bool IsEmpty(this ICecilBlock Block)
+        {
+            if (Block is EmptyBlock)
+            {
+                return true;
+            }
+            else if (Block is SequenceBlock)
+            {
+                var seq = (SequenceBlock)Block;
+                return seq.First.IsEmpty() && seq.Second.IsEmpty();
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
