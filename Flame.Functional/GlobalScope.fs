00cb85ae3853e27fbb29865f0d792191e19eb6f4
jonathanvdc <jonathan.vdc@outlook.com>
2015-08-13 00:18:33 +0200

    Started working toward Flame.Functional member access

--- Flame.Functional/GlobalScope.fs
+++ Flame.Functional/GlobalScope.fs
@@ -1,17 +1,23 @@
 ﻿namespace Flame.Functional
 
+open System
 open Flame
 open Flame.Compiler
 
 /// Defines a "global scope", i.e. a scope outside function.
-type GlobalScope(binder : IBinder, conversionRules : IConversionRules, log : ICompilerLog, namer : IType -> string) =
+type GlobalScope(binder : IBinder, conversionRules : IConversionRules, log : ICompilerLog, namer : IType -> string, memberProvider : IType -> ITypeMember seq) =
 
-    new(binder, conversionRules, log, namer : IConverter<IType, string>) =
-        GlobalScope(binder, conversionRules, log, namer.Convert)
+    new(binder, conversionRules, log, namer : IConverter<IType, string>, memberProvider : Func<IType, ITypeMember seq>) =
+        GlobalScope(binder, conversionRules, log, namer.Convert, memberProvider.Invoke)
 
     /// Gets this global scope's binder.
     member this.Binder =
         binder
+
+    /// Gets all instance, static and extension
+    /// type members for the given type.
+    member this.GetAllMembers =
+        memberProvider
 
     /// Gets this global scope's type namer.
     member this.TypeNamer =
