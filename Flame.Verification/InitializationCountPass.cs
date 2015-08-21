9347354d008c0f5410223cbac432949ab9cc327f
jonathanvdc <jonathan.vdc@outlook.com>
2015-08-21 12:43:12 +0200

    Made InitializationCountPass a little more modular

--- Flame.Verification/InitializationCountPass.cs
+++ Flame.Verification/InitializationCountPass.cs
@@ -56,11 +56,92 @@ namespace Flame.Verification
                    Log.UsePedanticWarnings(MaybeUninitializedWarningName) || Log.UsePedanticWarnings(MaybeMultipleInitializationWarningName);
         }
 
-        private static LogEntry AppendInitialization(LogEntry Entry, NodeCountVisitor Visitor)
+        /// <summary>
+        /// Appends the node count visitor's locations to the given entry as
+        /// initialization diagnostic remarks.
+        /// </summary>
+        /// <param name="Entry"></param>
+        /// <param name="Visitor"></param>
+        /// <returns></returns>
+        public static LogEntry AppendInitializationLocations(LogEntry Entry, NodeCountVisitor Visitor)
         {
             var newContents = Visitor.MatchLocations.Aggregate(Entry.Contents, 
                 (state, item) => RedefinitionHelpers.Instance.AppendDiagnosticsRemark(state, "Initialization: ", item));
             return new LogEntry(Entry.Name, newContents);
+        }
+
+        /// <summary>
+        /// Checks if the given visitor's current flow indicates possibly uninitialized flow,
+        /// and logs it if that is the case. A boolean is returned that tells if the control flow
+        /// is possibly uninitialized.
+        /// </summary>
+        /// <param name="Log"></param>
+        /// <param name="Target"></param>
+        /// <param name="Visitor"></param>
+        /// <returns></returns>
+        public static bool LogUninitialized(ICompilerLog Log, IMethod Target, NodeCountVisitor Visitor)
+        {
+            if (Visitor.CurrentFlow.Min == 0)
+            {
+                if (Visitor.CurrentFlow.Max == 0)
+                {
+                    var msg = new LogEntry("Instance uninitialized",
+                                           "The constructed instance is never initialized by this constructor. " +
+                                           Warnings.Instance.GetWarningNameMessage(UninitializedWarningName),
+                                           Target.GetSourceLocation());
+                    Log.LogWarning(AppendInitializationLocations(msg, Visitor));
+                }
+                else if (Log.UsePedanticWarnings(MaybeUninitializedWarningName))
+                {
+                    var msg = new LogEntry("Instance possibly uninitialized",
+                                           "Some control flow paths may not initialize the constructed instance. " +
+                                           Warnings.Instance.GetWarningNameMessage(MaybeUninitializedWarningName),
+                                           Target.GetSourceLocation());
+                    Log.LogWarning(AppendInitializationLocations(msg, Visitor));
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Checks if the given visitor's current flow indicates possibly multiply initialized flow,
+        /// and logs it if that is the case. A boolean is returned that tells if the control flow
+        /// is possibly multiply initialized.
+        /// </summary>
+        /// <param name="Log"></param>
+        /// <param name="Target"></param>
+        /// <param name="Visitor"></param>
+        /// <returns></returns>
+        public static bool LogMultipleInitialization(ICompilerLog Log, IMethod Target, NodeCountVisitor Visitor)
+        {
+            if (Visitor.CurrentFlow.Max > 1)
+            {
+                if (Visitor.CurrentFlow.Min > 1 && Log.UseDefaultWarnings(MultipleInitializationWarningName))
+                {
+                    var msg = new LogEntry("Instance initialized more than once",
+                                           "The constructed instance is initialized more than once by this constructor. " +
+                                           Warnings.Instance.GetWarningNameMessage(MultipleInitializationWarningName),
+                                           Target.GetSourceLocation());
+                    Log.LogWarning(AppendInitializationLocations(msg, Visitor));
+                }
+                else if (Log.UsePedanticWarnings(MaybeMultipleInitializationWarningName))
+                {
+                    var msg = new LogEntry("Instance possibly initialized more than once",
+                                           "The constructed instance may be initialized more than once in some control flow paths in this constructor. " +
+                                           Warnings.Instance.GetWarningNameMessage(MaybeMultipleInitializationWarningName),
+                                           Target.GetSourceLocation());
+                    Log.LogWarning(AppendInitializationLocations(msg, Visitor));
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public Tuple<IStatement, IMethod> Apply(Tuple<IStatement, IMethod> Value)
@@ -69,43 +150,9 @@ namespace Flame.Verification
             {
                 var visitor = InitializationCountHelpers.CreateVisitor();
                 visitor.Visit(Value.Item1);
-                if (visitor.CurrentFlow.Min == 0)
+                if (!LogUninitialized(Log, Value.Item2, visitor))
                 {
-                    if (visitor.CurrentFlow.Max == 0)
-                    {
-                        var msg = new LogEntry("Instance uninitialized",
-                                               "The constructed instance is never initialized by this constructor. " +
-                                               Warnings.Instance.GetWarningNameMessage(UninitializedWarningName),
-                                               Value.Item2.GetSourceLocation());
-                        Log.LogWarning(AppendInitialization(msg, visitor));
-                    }
-                    else if (Log.UsePedanticWarnings(MaybeUninitializedWarningName))
-                    {
-                        var msg = new LogEntry("Instance possibly uninitialized", 
-                                               "Some control flow paths may not initialize the constructed instance. " + 
-                                               Warnings.Instance.GetWarningNameMessage(MaybeUninitializedWarningName),
-                                               Value.Item2.GetSourceLocation());
-                        Log.LogWarning(AppendInitialization(msg, visitor));
-                    }
-                }
-                else if (visitor.CurrentFlow.Max > 1)
-                {
-                    if (visitor.CurrentFlow.Min > 1 && Log.UseDefaultWarnings(MultipleInitializationWarningName))
-                    {
-                        var msg = new LogEntry("Instance initialized more than once",
-                                               "The constructed instance is initialized more than once by this constructor. " +
-                                               Warnings.Instance.GetWarningNameMessage(MultipleInitializationWarningName),
-                                               Value.Item2.GetSourceLocation());
-                        Log.LogWarning(AppendInitialization(msg, visitor));
-                    }
-                    else if (Log.UsePedanticWarnings(MaybeMultipleInitializationWarningName))
-                    {
-                        var msg = new LogEntry("Instance possibly initialized more than once",
-                                               "The constructed instance may be initialized more than once in some control flow paths in this constructor. " +
-                                               Warnings.Instance.GetWarningNameMessage(MaybeMultipleInitializationWarningName),
-                                               Value.Item2.GetSourceLocation());
-                        Log.LogWarning(AppendInitialization(msg, visitor));
-                    }
+                    LogMultipleInitialization(Log, Value.Item2, visitor);
                 }
             }
             return Value;
