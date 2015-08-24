a23178e29af358786960f1fbe9f6946c5871abb4
jonathanvdc <jonathan.vdc@outlook.com>
2015-08-24 19:53:26 +0200

    FunctionalMethod uses functions instead of lazy arrays now

--- Flame.Functional/FunctionalMethod.fs
+++ Flame.Functional/FunctionalMethod.fs
@@ -10,29 +10,31 @@ open LazyHelpers
 /// Defines a functional-style method.
 type FunctionalMethod private(header : FunctionalMemberHeader, declType : IType,
                               isStatic : bool, isCtor : bool,
-                              genericParameters : LazyApplicationArray<IGenericMember, IGenericParameter>,
+                              genericParameters : IGenericMember -> IGenericParameter seq,
                               baseMethods : IMethod -> IMethod seq,
-                              returnType : IType Lazy, 
-                              parameters : IParameter LazyArrayBuilder,
+                              returnType : IMethod -> IType, 
+                              parameters : IMethod -> IParameter seq,
                               body : IMethod -> IStatement) as this =
 
     inherit FunctionalMemberBase(header, declType, isStatic)
 
-    let appliedGenericParams = genericParameters.ApplyLazy this
+    let appliedGenericParams = lazy genericParameters this
     let appliedBaseMethods = lazy (baseMethods this |> Seq.toArray)
-    let appliedBody = lazy (body this)
+    let appliedBody = lazy body this
+    let appliedRetType = lazy returnType this
+    let appliedParameters = lazy (parameters this |> Seq.toArray)
 
     new(header : FunctionalMemberHeader, declType : IType,
         isStatic : bool) =
         FunctionalMethod(header, declType, isStatic, false, 
-                         new LazyApplicationArray<IGenericMember, IGenericParameter>(),
                          (fun _ -> Seq.empty),
-                         new Lazy<IType>(fun _ -> PrimitiveTypes.Void),
-                         new LazyArrayBuilder<IParameter>(),
+                         (fun _ -> Seq.empty),
+                         (fun _ -> PrimitiveTypes.Void),
+                         (fun _ -> Seq.empty),
                          (fun _ -> null))
 
     /// Gets this functional-style method's return type.
-    member this.ReturnType = returnType.Value
+    member this.ReturnType = appliedRetType.Value
 
     /// Finds out if this functional-style method is a constructor or not.
     member this.IsConstructor = isCtor
@@ -41,7 +43,7 @@ type FunctionalMethod private(header : FunctionalMemberHeader, declType : IType,
     member this.GenericParameters = evalLazy appliedGenericParams
 
     /// Gets this functional-style method's parameters.
-    member this.Parameters = parameters.Value
+    member this.Parameters = appliedParameters.Value
 
     /// Gets this functional-style method's body statement, with lazy evaluation.
     member this.LazyBody = appliedBody
@@ -71,13 +73,13 @@ type FunctionalMethod private(header : FunctionalMemberHeader, declType : IType,
     member this.WithReturnType value =
         new FunctionalMethod(header, declType, isStatic, isCtor, genericParameters, baseMethods, value, parameters, body)
 
-    /// Adds this generic parameter to this function method.
-    member this.WithGenericParameter value =
-        new FunctionalMethod(header, declType, isStatic, isCtor, genericParameters.Append value, baseMethods, returnType, parameters, body)
+    /// Sets this functional method's generic parameters.
+    member this.WithGenericParameters value =
+        new FunctionalMethod(header, declType, isStatic, isCtor, value, baseMethods, returnType, parameters, body)
 
-    /// Adds this parameter to this functional method.
-    member this.WithParameter value =
-        new FunctionalMethod(header, declType, isStatic, isCtor, genericParameters, baseMethods, returnType, parameters.Append value, body)
+    /// Sets this functional method's parameters.
+    member this.WithParameters value =
+        new FunctionalMethod(header, declType, isStatic, isCtor, genericParameters, baseMethods, returnType, value, body)
 
     /// Sets this functional-style method's body.
     member this.WithBody value =
@@ -88,14 +90,14 @@ type FunctionalMethod private(header : FunctionalMemberHeader, declType : IType,
         new FunctionalMethod(header, declType, isStatic, isCtor, genericParameters, value, returnType, parameters, body)
 
     interface IMethod with
-        member this.ReturnType = returnType.Value
+        member this.ReturnType = this.ReturnType
         member this.GetParameters() = this.Parameters
         member this.IsConstructor = isCtor
         member this.GetBaseMethods() = this.BaseMethods
 
         member this.Invoke(target : IBoundObject, args : IBoundObject seq) = null // We don't do that yet.
 
-        member this.GetGenericParameters() = Seq.ofArray this.GenericParameters
+        member this.GetGenericParameters() = this.GenericParameters
         member this.GetGenericArguments() = Seq.empty
         member this.GetGenericDeclaration() = this :> IMethod
         member this.MakeGenericMethod tArgs = new DescribedGenericMethodInstance(this :> IMethod, new EmptyGenericResolver(), this.DeclaringType, tArgs) :> IMethod
