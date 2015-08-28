cf79aae38b1b03271ac468bad12b6ed018e911bd
jonathanvdc <jonathan.vdc@outlook.com>
2015-08-28 11:48:09 +0200

    Added support for "para" nodes

---
+++ Flame.Front/Cli/ParagraphWriter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flame.Front.Cli
+{
+    public class ParagraphWriter : INodeWriter
+    {
+        public ParagraphWriter(INodeWriter MainWriter)
+        {
+            this.MainWriter = MainWriter;
+        }
+
+        public INodeWriter MainWriter { get; private set; }
+
+        public void Write(Pixie.IMarkupNode Node, IConsole Console, IStylePalette Palette)
+        {
+            Console.WriteLine();
+            NodeWriter.WriteDefault(Node, Console, Palette, MainWriter);
+            Console.WriteLine();
+        }
+    }
+}
