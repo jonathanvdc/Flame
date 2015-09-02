f38e01a3fc37d730b2782e4c676d6df1456a3a9e
jonathanvdc <jonathan.vdc@outlook.com>
2015-09-02 16:33:49 +0200

    Added a type system test

---
+++ PassTests/TypeSystem.cs
@@ -0,0 +1,28 @@
+﻿using Flame;
+using Flame.Build;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassTests
+{
+    [TestClass]
+    public class TypeSystem
+    {
+        [TestMethod]
+        [TestCategory("Members - constructors")]
+        public void GetConstructors()
+        {
+            var descType = new DescribedType("X", null);
+            var descCtor = new DescribedMethod("this", descType);
+            descCtor.ReturnType = PrimitiveTypes.Void;
+            descCtor.IsConstructor = true;
+            descType.AddMethod(descCtor);
+
+            Assert.IsTrue(descType.GetConstructors().Count() == 1);
+        }
+    }
+}
