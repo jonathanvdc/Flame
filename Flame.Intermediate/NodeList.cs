976acd258a483abb7fd7f393620548e964b4a924
jonathanvdc <jonathan.vdc@outlook.com>
2015-09-14 18:04:25 +0200

    Added a generic node list structure

---
+++ Flame.Intermediate/NodeList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flame.Intermediate
+{
+    public class NodeList<T> : INodeStructure<IReadOnlyList<T>>
+    {
+        public NodeList(IReadOnlyList<INodeStructure<T>> Items)
+        {
+            this.Items = Items;
+        }
+
+        public IReadOnlyList<INodeStructure<T>> Items { get; private set; }
+
+        public Node Node
+        {
+            get { return NodeFactory.Block(Items.Select(item => item.Node).ToArray()); }
+        }
+
+        public IReadOnlyList<T> Value
+        {
+            get { return Items.Select(item => item.Value).ToArray(); }
+        }
+    }
+}
