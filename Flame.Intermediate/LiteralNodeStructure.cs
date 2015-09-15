9e71c13558a57e6a5b34d4d64eb51e50f92640e4
jonathanvdc <jonathan.vdc@outlook.com>
2015-09-15 10:56:51 +0200

    Added additional assembly ctors

---
+++ Flame.Intermediate/LiteralNodeStructure.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flame.Intermediate
+{
+    public class LiteralNodeStructure<T> : INodeStructure<T>
+    {
+        public LiteralNodeStructure(T Value)
+        {
+            this.Value = Value;
+        }
+
+        public T Value { get; private set; }
+
+        public Node Node
+        {
+            get { return NodeFactory.Literal(Value); }
+        }
+    }
+}
