5feed8051357fcad27e8694817b376dfbff0ed32
jonathanvdc <jonathan.vdc@outlook.com>
2015-09-15 11:52:30 +0200

    Added ParserState, IRParser

---
+++ Flame.Intermediate/Parsing/IRParser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flame.Intermediate.Parsing
+{
+    // IR file contents:
+    // - Type table
+    // - Method table
+    // - Field table
+    // - Assembly
+
+    public class IRParser
+    {
+        public IBinder ExternalBinder { get; private set; }
+    }
+}
