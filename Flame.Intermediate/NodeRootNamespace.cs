ea71680e70bc2362fccdb059a13c682dd12d177e
jonathanvdc <jonathan.vdc@outlook.com>
2015-09-15 10:53:00 +0200

    Added node assemblies

---
+++ Flame.Intermediate/NodeRootNamespace.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flame.Intermediate
+{
+    public class NodeRootNamespace : INamespaceBranch
+    {
+        public NodeRootNamespace(IAssembly DeclaringAssembly)
+        {
+            this.DeclaringAssembly = DeclaringAssembly;
+            this.TypeNodes = EmptyNodeList<IType>.Instance;
+            this.NamespaceNodes = EmptyNodeList<INamespaceBranch>.Instance;
+        }
+        public NodeRootNamespace(IAssembly DeclaringAssembly, INodeStructure<IEnumerable<IType>> TypeNodes, INodeStructure<IEnumerable<INamespaceBranch>> NamespaceNodes)
+        {
+            this.DeclaringAssembly = DeclaringAssembly;
+            this.TypeNodes = TypeNodes;
+            this.NamespaceNodes = NamespaceNodes;
+        }
+
+        public IAssembly DeclaringAssembly { get; private set; }
+        public INodeStructure<IEnumerable<IType>> TypeNodes { get; set; }
+        public INodeStructure<IEnumerable<INamespaceBranch>> NamespaceNodes { get; set; }
+
+        public IEnumerable<INamespaceBranch> Namespaces
+        {
+            get { return NamespaceNodes.Value; }
+        }
+
+        public IEnumerable<IAttribute> Attributes
+        {
+            get { return DeclaringAssembly.Attributes; }
+        }
+
+        public string FullName
+        {
+            get { return Name; }
+        }
+
+        public string Name
+        {
+            get { return ""; }
+        }
+
+        public IEnumerable<IType> Types
+        {
+            get { return TypeNodes.Value; }
+        }
+    }
+}
