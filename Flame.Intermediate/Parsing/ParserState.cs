7f179e50d787f0a73c87a49d6fa3997fe94a3d4d
jonathanvdc <jonathan.vdc@outlook.com>
2015-09-15 11:52:30 +0200

    Added ParserState, IRParser

---
+++ Flame.Intermediate/Parsing/ParserState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flame.Intermediate.Parsing
+{
+    public class ParserState
+    {
+        public ParserState(IBinder ExternalBinder, IReadOnlyList<INodeStructure<IType>> TypeTable,
+            IReadOnlyList<INodeStructure<IMethod>> MethodTable, IReadOnlyList<INodeStructure<IField>> FieldTable)
+        {
+            this.ExternalBinder = ExternalBinder;
+            this.TypeTable = TypeTable;
+            this.MethodTable = MethodTable;
+            this.FieldTable = FieldTable;
+        }
+
+        public IBinder ExternalBinder { get; private set; }
+        public IReadOnlyList<INodeStructure<IType>> TypeTable { get; private set; }
+        public IReadOnlyList<INodeStructure<IMethod>> MethodTable { get; private set; }
+        public IReadOnlyList<INodeStructure<IField>> FieldTable { get; private set; }
+    }
+}
