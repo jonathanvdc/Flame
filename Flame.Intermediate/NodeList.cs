be364cf417d42445146110d6958341321f33be99
jonathanvdc <jonathan.vdc@outlook.com>
2015-09-15 11:09:52 +0200

    NodeList caches results now

--- Flame.Intermediate/NodeList.cs
+++ Flame.Intermediate/NodeList.cs
@@ -11,6 +11,7 @@ namespace Flame.Intermediate
         public NodeList(IReadOnlyList<INodeStructure<T>> Items)
         {
             this.Items = Items;
+            this.cachedVal = new Lazy<T[]>(() => Items.Select(item => item.Value).ToArray());
         }
 
         public IReadOnlyList<INodeStructure<T>> Items { get; private set; }
@@ -20,9 +21,10 @@ namespace Flame.Intermediate
             get { return NodeFactory.Block(Items.Select(item => item.Node).ToArray()); }
         }
 
+        private Lazy<T[]> cachedVal;
         public IReadOnlyList<T> Value
         {
-            get { return Items.Select(item => item.Value).ToArray(); }
+            get { return cachedVal.Value; }
         }
     }
 }
