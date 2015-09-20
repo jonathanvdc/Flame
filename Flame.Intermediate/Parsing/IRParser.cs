b2f64d9a5dd364f08d413b3a1be6322e1bb55c5b
jonathanvdc <jonathan.vdc@outlook.com>
2015-09-20 15:55:36 +0200

    Minor IRParser refactor

--- Flame.Intermediate/Parsing/IRParser.cs
+++ Flame.Intermediate/Parsing/IRParser.cs
@@ -32,8 +32,8 @@ namespace Flame.Intermediate.Parsing
         }
 
         /// <summary>
-        /// Parses a top-level table of some kind. Said table is defined
-        /// by nodes of a specific type in the given sequence of nodes.
+        /// Parses a table of some kind. Said table is defined
+        /// by the concatenation of all nodes of a specific type in the given sequence of nodes.
         /// Each item is parsed by the given parsing function.
         /// </summary>
         /// <typeparam name="T"></typeparam>
@@ -44,7 +44,23 @@ namespace Flame.Intermediate.Parsing
         public static IReadOnlyList<T> ParseTable<T>(IEnumerable<LNode> Nodes, string TableType, Func<LNode, T> ParseEntry)
         {
             return GetTableEntries(Nodes, TableType).Select(ParseEntry)
-                                                           .ToArray();
+                                                    .ToArray();
+        }
+
+        /// <summary>
+        /// Parses a set of some kind. Said set is defined
+        /// by the concatenation of all nodes of a specific type in the given sequence of nodes.
+        /// Each item is parsed by the given parsing function.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="Nodes">The sequence of nodes to search for the given type of set.</param>
+        /// <param name="TableType">The identifier for the type of set to parse.</param>
+        /// <param name="ParseEntry">The parsing function for a set item.</param>
+        /// <returns></returns>
+        public static IEnumerable<T> ParseSet<T>(IEnumerable<LNode> Nodes, string TableType, Func<LNode, T> ParseEntry)
+        {
+            return GetTableEntries(Nodes, TableType).Select(ParseEntry)
+                                                    .Distinct();
         }
 
         /// <summary>
@@ -54,8 +70,7 @@ namespace Flame.Intermediate.Parsing
         /// <returns></returns>
         public static IEnumerable<string> ParseDependencies(IEnumerable<LNode> Nodes)
         {
-            return GetTableEntries(Nodes, DependencyNodeName).Select(item => item.Name.Name)
-                                                             .Distinct();
+            return ParseSet(Nodes, DependencyNodeName, item => item.Name.Name);
         }
     }
 }
