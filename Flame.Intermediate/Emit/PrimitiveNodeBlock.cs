c6788cb61bcb21867ef8933fd7efbde6738b6818
jonathanvdc <jonathan.vdc@outlook.com>
2015-10-15 18:30:25 +0200

    Started work on IRCodeGenerator

---
+++ Flame.Intermediate/Emit/PrimitiveNodeBlock.cs
@@ -0,0 +1,24 @@
+﻿using Flame.Compiler;
+using Loyc.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flame.Intermediate.Emit
+{
+    public class PrimitiveNodeBlock : INodeBlock
+    {
+        public PrimitiveNodeBlock(ICodeGenerator CodeGenerator, LNode Node, IType Type)
+        {
+            this.CodeGenerator = CodeGenerator;
+            this.Node = Node;
+            this.Type = Type;
+        }
+
+        public ICodeGenerator CodeGenerator { get; private set; }
+        public LNode Node { get; private set; }
+        public IType Type { get; private set; }
+    }
+}
