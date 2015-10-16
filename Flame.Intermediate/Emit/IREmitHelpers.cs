8d9a42d722d034633c0513b04470f766904bed14
jonathanvdc <jonathan.vdc@outlook.com>
2015-10-16 12:35:14 +0200

    Implemented attributes in IR back-end

--- Flame.Intermediate/Emit/IREmitHelpers.cs
+++ Flame.Intermediate/Emit/IREmitHelpers.cs
@@ -1,4 +1,7 @@
-﻿using Flame.Intermediate.Parsing;
+﻿using Flame.Build;
+using Flame.Compiler;
+using Flame.Intermediate.Parsing;
+using Loyc.Syntax;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,10 +19,72 @@ namespace Flame.Intermediate.Emit
 
         #region Attributes
 
+        private static readonly Dictionary<AccessModifier, string> accessAttributeNames = new Dictionary<AccessModifier, string>()
+        {
+            { AccessModifier.Public, AttributeParsers.PublicNodeName },
+            { AccessModifier.Private, AttributeParsers.PrivateNodeName },
+            { AccessModifier.Protected, AttributeParsers.ProtectedNodeName },
+            { AccessModifier.ProtectedOrAssembly, AttributeParsers.ProtectedOrInternal },
+            { AccessModifier.ProtectedAndAssembly, AttributeParsers.ProtectedAndInternal },
+            { AccessModifier.Assembly, AttributeParsers.InternalNodeName },
+        };
+
+        private static readonly Dictionary<IAttribute, string> constantAttributeNames = new Dictionary<IAttribute, string>()
+        {
+            // Miscellaneous attributes
+            { PrimitiveAttributes.Instance.ConstantAttribute, AttributeParsers.ConstantNodeName },
+            { PrimitiveAttributes.Instance.ExtensionAttribute, AttributeParsers.ExtensionNodeName },
+            { PrimitiveAttributes.Instance.HiddenAttribute, AttributeParsers.HiddenNodeName },
+
+            // Inheritance attributes
+            { PrimitiveAttributes.Instance.AbstractAttribute, AttributeParsers.AbstractNodeName },
+            { PrimitiveAttributes.Instance.VirtualAttribute, AttributeParsers.VirtualNodeName },
+
+            // Type attributes
+            { PrimitiveAttributes.Instance.StaticTypeAttribute, AttributeParsers.StaticTypeNodeName },
+            { PrimitiveAttributes.Instance.ReferenceTypeAttribute, AttributeParsers.ReferenceTypeNodeName },
+            { PrimitiveAttributes.Instance.ValueTypeAttribute, AttributeParsers.ValueTypeNodeName },
+            { PrimitiveAttributes.Instance.InterfaceAttribute, AttributeParsers.InterfaceTypeNodeName }
+        };
+
         public static INodeStructure<IAttribute> ConvertAttribute(IRAssemblyBuilder Assembly, IAttribute Attribute)
         {
-            // TODO: implement this!
-            throw new NotImplementedException();
+            if (Attribute is AccessAttribute)
+            {
+                var mod = ((AccessAttribute)Attribute).Access;
+                return new ConstantNodeStructure<IAttribute>(NodeFactory.Id(accessAttributeNames[mod]), Attribute);
+            }
+            else if (Attribute is IConstructedAttribute)
+            {
+                // Format:
+                //
+                // #attribute(<attribute_constructor>, <argument_expressions...>)
+
+                var ctedAttr = (IConstructedAttribute)Attribute;
+                var attrCtor = Assembly.MethodTable.GetReference(ctedAttr.Constructor);
+                var args = ConvertExpressions(Assembly, ctedAttr.GetArguments().Select(PrimitiveExpressionExtensions.ToExpression));
+                return new ConstantNodeStructure<IAttribute>(NodeFactory.Call(AttributeParsers.ConstructedAttributeNodeName,
+                    new[] { attrCtor }.Concat(args)),
+                    Attribute);
+            }
+            else
+            {
+                string attrName;
+                if (constantAttributeNames.TryGetValue(Attribute, out attrName))
+                {
+                    return new ConstantNodeStructure<IAttribute>(NodeFactory.Id(attrName), Attribute);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static IEnumerable<INodeStructure<IAttribute>> ConvertAttributes(IRAssemblyBuilder Assembly, IEnumerable<IAttribute> Attributes)
+        {
+            return Attributes.Select(item => ConvertAttribute(Assembly, item))
+                             .Where(item => item != null);
         }
 
         #endregion
@@ -79,6 +144,41 @@ namespace Flame.Intermediate.Emit
             return new IRParameter(
                 CreateSignature(Assembly, Parameter.Name, Parameter.Attributes), 
                 Assembly.TypeTable.GetReferenceStructure(Parameter.ParameterType));
+        }
+
+        #endregion
+
+        #region Expressions
+
+        public static LNode ConvertExpression(IRAssemblyBuilder Assembly, IExpression Expression)
+        {
+            return ConvertExpression(Assembly, Expression, (IType)null);
+        }
+
+        public static IEnumerable<LNode> ConvertExpressions(IRAssemblyBuilder Assembly, IEnumerable<IExpression> Expressions)
+        {
+            return ConvertExpressions(Assembly, Expressions, (IType)null);
+        }
+
+        public static LNode ConvertExpression(IRAssemblyBuilder Assembly, IExpression Expression, IType DeclaringType)
+        {
+            var descMethod = new DescribedMethod("", DeclaringType, PrimitiveTypes.Void, true);
+
+            return ConvertExpression(Assembly, Expression, descMethod);
+        }
+
+        public static IEnumerable<LNode> ConvertExpressions(IRAssemblyBuilder Assembly, IEnumerable<IExpression> Expressions, IType DeclaringType)
+        {
+            var descMethod = new DescribedMethod("", DeclaringType, PrimitiveTypes.Void, true);
+
+            return Expressions.Select(item => ConvertExpression(Assembly, item, descMethod));
+        }
+
+        public static LNode ConvertExpression(IRAssemblyBuilder Assembly, IExpression Expression, IMethod DeclaringMethod)
+        {
+            var codeGen = new IRCodeGenerator(Assembly, DeclaringMethod);
+
+            return NodeBlock.ToNode(codeGen.Postprocess(Expression.Emit(codeGen)));
         }
 
         #endregion
