f47061cddb0a4aa9d26d3aeee2e93032a113d69c
jonathanvdc <jonathan.vdc@outlook.com>
2015-10-24 14:28:02 +0200

    Implemented INamespaceBuilder for IRTypeBuilder

--- Flame.Intermediate/Emit/IRTypeBuilder.cs
+++ Flame.Intermediate/Emit/IRTypeBuilder.cs
@@ -1,4 +1,5 @@
-﻿using Flame.Compiler.Build;
+﻿using Flame.Build;
+using Flame.Compiler.Build;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Flame.Intermediate.Emit
 {
-    public class IRTypeBuilder : IRTypeDefinition, ITypeBuilder
+    public class IRTypeBuilder : IRTypeDefinition, ITypeBuilder, INamespaceBuilder
     {
         public IRTypeBuilder(IRAssemblyBuilder Assembly, INamespace DeclaringNamespace, ITypeSignatureTemplate Template)
             : base(DeclaringNamespace, new IRSignature(Template.Name))
@@ -22,6 +23,11 @@ namespace Flame.Intermediate.Emit
         private void AddMember(INodeStructure<ITypeMember> Member)
         {
             this.MemberNodes = new NodeCons<ITypeMember>(Member, this.MemberNodes);
+        }
+
+        private void AddNestedType(INodeStructure<IType> NestedType)
+        {
+            this.NestedTypeNodes = new NodeCons<IType>(NestedType, this.NestedTypeNodes);
         }
 
         public IFieldBuilder DeclareField(IFieldSignatureTemplate Template)
@@ -45,7 +51,28 @@ namespace Flame.Intermediate.Emit
             return property;
         }
 
+        public INamespaceBuilder DeclareNamespace(string Name)
+        {
+            var prototype = new DescribedType(Name, this);
+            prototype.AddAttribute(PrimitiveAttributes.Instance.StaticTypeAttribute);
+            var ty = new IRTypeBuilder(Assembly, this, new TypePrototypeTemplate(prototype));
+            AddNestedType(ty);
+            return ty;
+        }
+
+        public ITypeBuilder DeclareType(ITypeSignatureTemplate Template)
+        {
+            var ty = new IRTypeBuilder(Assembly, this, Template);
+            AddNestedType(ty);
+            return ty;
+        }
+
         public IType Build()
+        {
+            return this;
+        }
+
+        INamespace IMemberBuilder<INamespace>.Build()
         {
             return this;
         }
