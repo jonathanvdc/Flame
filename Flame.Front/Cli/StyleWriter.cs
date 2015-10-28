b7b403e955ca67ac0b4e4809b899f9c0f2d42ed4
jonathanvdc <jonathan.vdc@outlook.com>
2015-10-28 16:58:08 +0100

    Modified some fatal error messages

---
+++ Flame.Front/Cli/StyleWriter.cs
@@ -0,0 +1,38 @@
+﻿using Pixie;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flame.Front.Cli
+{
+    /// <summary>
+    /// A type of node writer that prints a node's 
+    /// contents in a specific style. 
+    /// </summary>
+    public class StyleWriter : INodeWriter
+    {
+        public StyleWriter(INodeWriter MainWriter, Style NodeStyle)
+        {
+            this.MainWriter = MainWriter;
+            this.NodeStyle = NodeStyle;
+        }
+
+        public INodeWriter MainWriter { get; private set; }
+        public Style NodeStyle { get; private set; }
+
+        public void Write(IMarkupNode Node, IConsole Console, IStylePalette Palette)
+        {
+            Console.PushStyle(Node.GetStyle(Palette));
+            Console.PushStyle(NodeStyle);
+            Console.Write(Node.GetText());
+            foreach (var item in Node.Children)
+            {
+                MainWriter.Write(item, Console, Palette);
+            }
+            Console.PopStyle();
+            Console.PopStyle();
+        }
+    }
+}
