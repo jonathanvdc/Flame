706e99cc94b6dea48fb1ed6a78496f5eb1494d32
jonathanvdc <jonathan.vdc@outlook.com>
2015-10-29 17:17:50 +0100

    Added a 'cause' node type

---
+++ Flame.Front/Cli/CauseNodeWriter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pixie;
+
+namespace Flame.Front.Cli
+{
+    public class CauseNodeWriter : INodeWriter
+    {
+        public CauseNodeWriter(INodeWriter MainWriter)
+        {
+            this.MainWriter = MainWriter;
+        }
+
+        public INodeWriter MainWriter { get; private set; }
+
+        public void Write(IMarkupNode Node, IConsole Console, IStylePalette Palette)
+        {
+            Console.PushStyle(Node.GetStyle(Palette));
+            Console.Write("[-");
+            Console.Write(Node.GetText());
+            foreach (var item in Node.Children)
+            {
+                MainWriter.Write(item, Console, Palette);
+            }
+            Console.Write("]");
+            Console.PopStyle();
+        }
+    }
+}
