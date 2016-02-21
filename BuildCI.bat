4922a45123f3251a9f1da065b9817227470b271e
jonathanvdc <jonathan.vdc@outlook.com>
2016-02-21 20:27:08 +0100

    Made some changes to the AppVeyor build

--- BuildCI.bat
+++ BuildCI.bat
@@ -1,12 +1,12 @@
 
-%1 Pixie/Pixie.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug
-%1 Pixie/Pixie.Xml.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug
-%1 Flame/Flame.dsc.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug
-%1 Flame.Compiler/Flame.Compiler.dsc.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug
-%1 Flame.Optimization/Flame.Optimization.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug
-%1 Flame.Syntax/Flame.Syntax.dsc.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug
-%1 Flame.Markdown/Flame.Markdown.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug
-%1 Flame.DSharp/Flame.DSharp.dsc.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug
+%1 Pixie/Pixie.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command
+%1 Pixie/Pixie.Xml.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command
+%1 Flame/Flame.dsc.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command
+%1 Flame.Compiler/Flame.Compiler.dsc.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command
+%1 Flame.Optimization/Flame.Optimization.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command
+%1 Flame.Syntax/Flame.Syntax.dsc.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command
+%1 Flame.Markdown/Flame.Markdown.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command
+%1 Flame.DSharp/Flame.DSharp.dsc.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command
 cd ./Flame.Cecil/
 msbuild /p:Configuration=Release Flame.Cecil.mono.sln
 cd ..
