1fa4545ec94c907dc89b8ccf9c066217c031ea3e
jonathanvdc <jonathan.vdc@outlook.com>
2016-02-21 19:18:52 +0100

    Created a .travis.yml file

---
+++ BuildCI.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+shopt -s expand_aliases
+
+alias dsc="mono $1"
+
+dsc Pixie/Pixie.dsproj -Wall -Wextra -pedantic -O2 -repeat-command -time -g --debug
+dsc Pixie/Pixie.Xml.dsproj -Wall -Wextra -pedantic -O2 -repeat-command -time -g --debug
+dsc Flame/Flame.dsc.dsproj -Wall -Wextra -pedantic -O2 -repeat-command -time -g --debug
+dsc Flame.Compiler/Flame.Compiler.dsc.dsproj -Wall -Wextra -pedantic -O2 -repeat-command -time -g --debug
+dsc Flame.Optimization/Flame.Optimization.dsproj -Wall -Wextra -pedantic -O2 -repeat-command -time -g --debug
+dsc Flame.Syntax/Flame.Syntax.dsc.dsproj -Wall -Wextra -pedantic -O2 -repeat-command -time -g --debug
+dsc Flame.Markdown/Flame.Markdown.dsproj -Wall -Wextra -pedantic -O2 -repeat-command -time -g --debug
+dsc Flame.DSharp/Flame.DSharp.dsc.dsproj -Wall -Wextra -pedantic -O2 -repeat-command -time -g --debug
+cd ./Flame.Cecil/
+xbuild /p:Configuration=Release Flame.Cecil.mono.sln
+cd ..
