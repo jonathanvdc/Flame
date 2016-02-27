7fd55a845a8f59b8f16b9492fd4b86bf87396e88
jonathanvdc <jonathan.vdc@outlook.com>
2016-02-27 13:37:10 +0100

    Created module/namespace/type classes

--- Flame.Wasm/WasmHelpers.cs
+++ Flame.Wasm/WasmHelpers.cs
@@ -1,21 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Flame.Wasm
 {
 	public static class WasmHelpers
 	{
-		public static string MangleName(IType Type)
+		private static readonly Dictionary<IType, string> scalarWasmNames = new Dictionary<IType, string>()
+		{
+			{ PrimitiveTypes.Int8, "i32" },
+			{ PrimitiveTypes.Int16, "i32" },
+			{ PrimitiveTypes.Int32, "i32" },
+			{ PrimitiveTypes.Int64, "i64" },
+			{ PrimitiveTypes.UInt8, "i32" },
+			{ PrimitiveTypes.UInt16, "i32" },
+			{ PrimitiveTypes.UInt32, "i32" },
+			{ PrimitiveTypes.UInt64, "i64" },
+			{ PrimitiveTypes.Bit8, "i32" },
+			{ PrimitiveTypes.Bit16, "i32" },
+			{ PrimitiveTypes.Bit32, "i32" },
+			{ PrimitiveTypes.Bit64, "i64" },
+			{ PrimitiveTypes.Boolean, "i32" },
+			{ PrimitiveTypes.Char, "i32" }
+		};
+
+		public static string GetScalarWasmName(IType Type)
+		{
+			if (Type.GetIsPointer())
+				return "i32";
+			else
+				return scalarWasmNames[Type];
+		}
+
+		public static string GetWasmName(IType Type)
 		{
 			if (Type == null)
 				return "";
 			return Type.FullName;
 		}
 
-		public static string MangleName(IMethod Method)
+		public static string GetWasmName(IMethod Method)
 		{
 			if (Method == null)
 				return "";
-			return MemberExtensions.CombineNames(MangleName(Method.DeclaringType), Method.Name);
+			return MemberExtensions.CombineNames(GetWasmName(Method.DeclaringType), Method.Name);
 		}
 
 		public static bool IsScalar(this IType Type)
