c2b32d359a24055f5af05acd342050ae46b9b5af
jonathanvdc <jonathan.vdc@outlook.com>
2016-02-28 16:59:11 +0100

    A number of passes use register variables now

--- BuildFlame.sh
+++ BuildFlame.sh
@@ -1,7 +1,7 @@
 # This script builds the Flame core libraries with dsc, and then
 # proceeds to call xbuild on the peripheral Flame libraries.
 
-bash ./BuildFlameCore.sh
+bash ./BuildFlameCore.sh $@
 cd ./Flame.Cecil/
 xbuild /p:Configuration=Release Flame.Cecil.mono.sln
 cd ..
