4d3644a012c73363f5ae919b0347c5ebfc41b128
jonathanvdc <jonathan.vdc@outlook.com>
2016-03-13 13:31:41 +0100

    Wasm -O3 tries harder now

--- Flame.Front.Common/Target/WasmBuildTargetParser.cs
+++ Flame.Front.Common/Target/WasmBuildTargetParser.cs
@@ -16,6 +16,13 @@ using Flame.Optimization.Relooper;
 
 namespace Flame.Front.Target
 {
+	using MethodPassInfo = PassInfo<BodyPassArgument, IStatement>;
+    using SignaturePassInfo = PassInfo<MemberSignaturePassArgument<IMember>, MemberSignaturePassResult>;
+    using StatementPassInfo = PassInfo<IStatement, IStatement>;
+    using RootPassInfo = PassInfo<BodyPassArgument, IEnumerable<IMember>>;
+    using IRootPass = IPass<BodyPassArgument, IEnumerable<IMember>>;
+    using ISignaturePass = IPass<MemberSignaturePassArgument<IMember>, MemberSignaturePassResult>;
+
 	public class WasmBuildTargetParser : IBuildTargetParser
 	{
 		public const string WasmIdentifier = "wasm";
@@ -75,18 +82,54 @@ namespace Flame.Front.Target
             extraPasses.RegisterLoweringPass(new PassInfo<BodyPassArgument, IStatement>(
                 new ProloguePass(abi), ProloguePass.ProloguePassName));
             extraPasses.RegisterPassCondition(new PassCondition(ProloguePass.ProloguePassName, optInfo => true));
+
+			// Perform some final correctness optimizations.
 			// -flower-copy
 			extraPasses.RegisterLoweringPass(new PassInfo<IStatement, IStatement>(
 				new CopyLoweringPass(abi), CopyLoweringPass.CopyLoweringPassName));
 			extraPasses.RegisterPassCondition(new PassCondition(CopyLoweringPass.CopyLoweringPassName, optInfo => true));
 
+			// -frewrite-return-void
+			extraPasses.RegisterLoweringPass(new PassInfo<BodyPassArgument, IStatement>(
+				RewriteVoidReturnPass.Instance, RewriteVoidReturnPass.RewriteVoidReturnPassName));
+			// extraPasses.RegisterPassCondition(new PassCondition(RewriteVoidReturnPass.RewriteVoidReturnPassName, optInfo => true));
+
 			// These passes perform optimizations _after_ the correctness passes have
 			// lowered a number of constructs.
 
-            // -frewrite-return-void
-            extraPasses.RegisterLoweringPass(new PassInfo<BodyPassArgument, IStatement>(
-                RewriteVoidReturnPass.Instance, RewriteVoidReturnPass.RewriteVoidReturnPassName));
-            // extraPasses.RegisterPassCondition(new PassCondition(RewriteVoidReturnPass.RewriteVoidReturnPassName, optInfo => true));
+			// Insert some CFG/SSA optimizations here to optimize low-level
+			// wasm code.
+
+			// run -fconstruct-cfg, because the lowering constructions may have
+			// inserted additional code that does not respect the control-flow graph.
+			extraPasses.RegisterLoweringPass(new StatementPassInfo(ConstructFlowGraphPass.Instance, ConstructFlowGraphPass.ConstructFlowGraphPassName + "-lowered"));
+			extraPasses.RegisterPassCondition(ConstructFlowGraphPass.ConstructFlowGraphPassName + "-lowered", optInfo => optInfo.OptimizeAggressive);
+
+			// run these passes to get a decent CFG
+			extraPasses.RegisterLoweringPass(new StatementPassInfo(SimplifySelectFlowPass.Instance, SimplifySelectFlowPass.SimplifySelectFlowPassName + "-lowered"));
+			extraPasses.RegisterPassCondition(SimplifySelectFlowPass.SimplifySelectFlowPassName + "-lowered", optInfo => optInfo.OptimizeAggressive);
+			extraPasses.RegisterLoweringPass(new StatementPassInfo(JumpThreadingPass.Instance, JumpThreadingPass.JumpThreadingPassName + "-lowered"));
+			extraPasses.RegisterPassCondition(JumpThreadingPass.JumpThreadingPassName + "-lowered", optInfo => optInfo.OptimizeAggressive);
+			extraPasses.RegisterLoweringPass(new MethodPassInfo(DeadBlockEliminationPass.Instance, DeadBlockEliminationPass.DeadBlockEliminationPassName + "-lowered"));
+			extraPasses.RegisterPassCondition(DeadBlockEliminationPass.DeadBlockEliminationPassName + "-lowered", optInfo => optInfo.OptimizeAggressive);
+
+			// run -fconstruct-ssa, because the lowering constructions use
+			// register variables instead of SSA variables.
+			extraPasses.RegisterLoweringPass(new MethodPassInfo(ConstructSSAPass.Instance, ConstructSSAPass.ConstructSSAPassName + "-lowered"));
+			extraPasses.RegisterPassCondition(ConstructSSAPass.ConstructSSAPassName + "-lowered", optInfo => optInfo.OptimizeAggressive);
+
+			// -fcopyprop should get rid of aliased variables
+			extraPasses.RegisterLoweringPass(new MethodPassInfo(CopyPropagationPass.Instance, CopyPropagationPass.CopyPropagationPassName + "-lowered"));
+			extraPasses.RegisterPassCondition(CopyPropagationPass.CopyPropagationPassName + "-lowered", optInfo => optInfo.OptimizeAggressive);
+
+			// -fglobal-valueprop may be useful here, too.
+			extraPasses.RegisterLoweringPass(new MethodPassInfo(GlobalValuePropagationPass.Instance, GlobalValuePropagationPass.GlobalValuePropagationPassName + "-lowered"));
+			extraPasses.RegisterPassCondition(GlobalValuePropagationPass.GlobalValuePropagationPassName + "-lowered", optInfo => optInfo.OptimizeAggressive);
+
+			// -fdead-store-elimination should kill the -fprologue construction
+			// if it was redundant.
+			extraPasses.RegisterLoweringPass(new StatementPassInfo(DeadStoreEliminationPass.Instance, DeadStoreEliminationPass.DeadStoreEliminationPassName + "-lowered"));
+			extraPasses.RegisterPassCondition(DeadStoreEliminationPass.DeadStoreEliminationPassName + "-lowered", optInfo => optInfo.OptimizeAggressive);
 
 			// -foptimize-nodes-lowered
 			extraPasses.RegisterLoweringPass(new PassInfo<IStatement, IStatement>(
