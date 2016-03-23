0fa9bc272fe39571f160568ab0382686ca6633ec
jonathanvdc <jonathan.vdc@outlook.com>
2016-03-23 23:46:12 +0100

    Updated inlining heuristics

--- Flame.Front.Common/Target/ScalarReplacementPass.cs
+++ Flame.Front.Common/Target/ScalarReplacementPass.cs
@@ -10,6 +10,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics.Contracts;
 using Flame.Front.Passes;
+using Flame.Compiler.Expressions;
+using Flame.Compiler.Variables;
+using Flame.Compiler.Statements;
 
 namespace Flame.Front.Target
 {
@@ -37,6 +40,102 @@ namespace Flame.Front.Target
         public override int GetMaxRecursion(BodyPassArgument Argument)
         {
             return 1;
+        }
+    }
+
+    /// <summary>
+    /// A node visitor that computes the relative cost of
+    /// inlining a method, provided that its caller is
+    /// a viable candidate for scalar replacement.
+    /// </summary>
+    public sealed class ScalarReplacementSizeVisitor : SizeVisitor
+    {
+        public ScalarReplacementSizeVisitor(Func<IType, bool> IsScalarReplacementCandidate)
+            : base(true)
+        {
+            this.IsScalarReplacementCandidate = IsScalarReplacementCandidate;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the given type is a candidate for scalar replacement.
+        /// </summary>
+        /// <value><c>true</c> if the given type is a scalar replacement candidate; otherwise, <c>false</c>.</value>
+        public Func<IType, bool> IsScalarReplacementCandidate { get; private set; }
+
+        private bool IsLocalOrArgVariable(IExpression Expr)
+        {
+            var varExpr = Expr.GetEssentialExpression() as IVariableNode;
+            var srcVar = varExpr.GetVariable();
+            return srcVar is ThisVariable 
+                || srcVar is ArgumentVariable
+                || srcVar is LocalVariableBase;
+        }
+
+        private static int ApproximateAccessCost(IType Type)
+        {
+            // Assume that loads to sub-word-size memory locations
+            // are just as expensive as loading a word.
+            return Math.Max(InliningPass.WordSize, InliningPass.ApproximateSize(Type)) / 2;
+        }
+
+        /// <summary>
+        /// "Visits" an expression: an expression is taken as input and transformed another expression.
+        /// </summary>
+        public override IExpression Visit(IExpression Value)
+        {
+            if (Value is FieldGetExpression)
+            {
+                var fieldGet = (FieldGetExpression)Value;
+                if (IsLocalOrArgVariable(fieldGet.Target)
+                    && IsScalarReplacementCandidate(fieldGet.Field.DeclaringType))
+                {
+                    // If this function were to get inlined and the aggregate
+                    // gets replaced by scalars, then the field can be kept
+                    // in a register. Being optimistic here could result in some
+                    // really good results.
+                    Size -= ApproximateAccessCost(fieldGet.Field.FieldType);
+                    return fieldGet;
+                }
+            }
+            else if (Value is FieldGetPointerExpression)
+            {
+                var fieldPtr = (FieldGetPointerExpression)Value;
+                if (IsLocalOrArgVariable(fieldPtr.Target)
+                    && IsScalarReplacementCandidate(fieldPtr.Field.DeclaringType))
+                {
+                    // If this function were to get inlined and the aggregate
+                    // gets replaced by scalars, then the field can be kept
+                    // in a register. Being optimistic here could result in some
+                    // really good results.
+                    Size -= ApproximateAccessCost(fieldPtr.Field.FieldType);
+                    return fieldPtr;
+                }
+            }
+            return base.Visit(Value);
+        }
+
+        /// <summary>
+        /// "Visits" a statement: an statement is taken as input and transformed another statement.
+        /// </summary>
+        public override IStatement Visit(IStatement Value)
+        {
+            if (Value is FieldSetStatement)
+            {
+                var fieldSet = (FieldSetStatement)Value;
+                if (IsLocalOrArgVariable(fieldSet.Target)
+                    && IsScalarReplacementCandidate(fieldSet.Field.DeclaringType))
+                {
+                    // If this function were to get inlined and the aggregate
+                    // gets replaced by scalars, then the field can be kept
+                    // in a register. Being optimistic here could result in some
+                    // really good results.
+                    Size -= ApproximateAccessCost(fieldSet.Field.FieldType);
+                    // We do, however, have to consider the field's value.
+                    Visit(fieldSet.Value);
+                    return fieldSet;
+                }
+            }
+            return base.Visit(Value);
         }
     }
 
@@ -97,7 +196,17 @@ namespace Flame.Front.Target
             var log = Argument.PassEnvironment.Log;
 
             int inlineTolerance = GetInlineTolerance(log.Options);
-            return call => InliningPass.ShouldInline(Argument, call, inlineTolerance, false);
+            var replCriteria = GetReplacementCriteria(Argument);
+            Func<IType, bool> canRepl = ty => ty.GetIsValueType() && replCriteria(ty);
+            return call => InliningPass.ShouldInline(
+                Argument, call, 
+                body => 
+                {
+                    var visitor = new ScalarReplacementSizeVisitor(canRepl);
+                    visitor.Visit(body);
+                    return visitor.Size - inlineTolerance;
+                },
+                false);
         }
 
         public override int GetMaxRecursion(BodyPassArgument Argument)
