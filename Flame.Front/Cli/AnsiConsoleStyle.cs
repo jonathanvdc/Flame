636f8f969dbc412a1d9456ce625bcc0cbfdc0926
jonathanvdc <jonathan.vdc@outlook.com>
2016-04-06 21:19:50 +0200

    Tweaked ANSI color code selection to better suit black-on-white console output

--- Flame.Front/Cli/AnsiConsoleStyle.cs
+++ Flame.Front/Cli/AnsiConsoleStyle.cs
@@ -41,39 +41,33 @@ namespace Flame.Front.Cli
             Console.Write(GetEscapeSequence(OldStyle, InitialStyle));
         }
 
-        static AnsiConsoleStyle()
-        {
-            colorIdents = new Dictionary<ConsoleColor, int>();
-            colorIdents[ConsoleColor.Black] = 0;
-            colorIdents[ConsoleColor.DarkRed] = 1;
-            colorIdents[ConsoleColor.DarkGreen] = 2;
-            colorIdents[ConsoleColor.DarkYellow] = 3;
-            colorIdents[ConsoleColor.DarkBlue] = 4;
-            colorIdents[ConsoleColor.DarkMagenta] = 5;
-            colorIdents[ConsoleColor.DarkCyan] = 6;
-            colorIdents[ConsoleColor.Gray] = 7;
-            colorIdents[ConsoleColor.DarkGray] = 0;
-            colorIdents[ConsoleColor.Red] = 1;
-            colorIdents[ConsoleColor.Green] = 2;
-            colorIdents[ConsoleColor.Yellow] = 3;
-            colorIdents[ConsoleColor.Blue] = 4;
-            colorIdents[ConsoleColor.Magenta] = 5;
-            colorIdents[ConsoleColor.Cyan] = 6;
-            colorIdents[ConsoleColor.White] = 7;
+		private static readonly Dictionary<ConsoleColor, int> colorIdents = new Dictionary<ConsoleColor, int>()
+		{
+			{ ConsoleColor.Black, 0 },
+			{ ConsoleColor.DarkRed, 1 },
+			{ ConsoleColor.DarkGreen, 2 },
+			{ ConsoleColor.DarkYellow, 3 },
+			{ ConsoleColor.DarkBlue, 4 },
+			{ ConsoleColor.DarkMagenta, 5 },
+			{ ConsoleColor.DarkCyan, 6 },
+			{ ConsoleColor.Gray, 7 },
+			{ ConsoleColor.DarkGray, 0 },
+			{ ConsoleColor.Red, 1 },
+			{ ConsoleColor.Green, 2 },
+			{ ConsoleColor.Yellow, 3 },
+			{ ConsoleColor.Blue, 4 },
+			{ ConsoleColor.Magenta, 5 },
+			{ ConsoleColor.Cyan, 6 },
+			{ ConsoleColor.White, 7 }
+		};
 
-            boldColors = new HashSet<ConsoleColor>();
-            boldColors.Add(ConsoleColor.DarkGray);
-            boldColors.Add(ConsoleColor.Red);
-            boldColors.Add(ConsoleColor.Green);
-            boldColors.Add(ConsoleColor.Yellow);
-            boldColors.Add(ConsoleColor.Blue);
-            boldColors.Add(ConsoleColor.Magenta);
-            boldColors.Add(ConsoleColor.Cyan);
-            boldColors.Add(ConsoleColor.White);
-        }
-
-        private static Dictionary<ConsoleColor, int> colorIdents;
-        private static HashSet<ConsoleColor> boldColors;
+		private static readonly HashSet<ConsoleColor> boldColors = new HashSet<ConsoleColor>()
+		{
+			ConsoleColor.DarkGray, ConsoleColor.Red,
+			ConsoleColor.Green, ConsoleColor.Yellow,
+			ConsoleColor.Blue, ConsoleColor.Magenta,
+			ConsoleColor.Cyan, ConsoleColor.White
+		};
 
         public string GetEscapeSequence(AnsiConsoleStyle OldStyle, AnsiConsoleStyle InitialStyle)
         {
@@ -94,13 +88,17 @@ namespace Flame.Front.Cli
             }
             if (!ResetAll)
             {
-                if (reset && (ForegroundConsoleColor != InitialStyle.ForegroundConsoleColor) || ForegroundConsoleColor != OldStyle.ForegroundConsoleColor)
+				int oldFgIdent = colorIdents[OldStyle.ForegroundConsoleColor];
+				int oldBgIdent = colorIdents[OldStyle.BackgroundConsoleColor];
+				int newFgIdent = colorIdents[ForegroundConsoleColor];
+				int newBgIdent = colorIdents[BackgroundConsoleColor];
+				if (reset && (newFgIdent != colorIdents[InitialStyle.ForegroundConsoleColor]) || newFgIdent != oldFgIdent)
                 {
-                    codes.Add("3" + colorIdents[ForegroundConsoleColor].ToString(CultureInfo.InvariantCulture));
+					codes.Add("3" + newFgIdent.ToString(CultureInfo.InvariantCulture));
                 }
-                if (reset && (BackgroundConsoleColor != InitialStyle.BackgroundConsoleColor) || BackgroundConsoleColor != OldStyle.BackgroundConsoleColor)
+				if (reset && (newBgIdent != colorIdents[InitialStyle.BackgroundConsoleColor]) || newBgIdent != oldBgIdent)
                 {
-                    codes.Add("4" + colorIdents[BackgroundConsoleColor].ToString(CultureInfo.InvariantCulture));
+					codes.Add("4" + newBgIdent.ToString(CultureInfo.InvariantCulture));
                 }
             }
 
