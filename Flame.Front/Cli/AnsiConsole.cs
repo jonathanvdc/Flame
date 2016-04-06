569e421fe84d8ebd7275269ff752eee30f6be9cc
jonathanvdc <jonathan.vdc@outlook.com>
2016-04-06 21:07:37 +0200

    ANSI escape code console makes its own color palette assumptions now

--- Flame.Front/Cli/AnsiConsole.cs
+++ Flame.Front/Cli/AnsiConsole.cs
@@ -24,7 +24,13 @@ namespace Flame.Front.Cli
         {
             get
             {
-                return DefaultConsole.DefaultBackgroundColor;
+				// Mono seems to default to black as background color,
+				// no matter what the terminal color is actually set to.
+				// This makes asking the Console class just about 
+				// useless. Let's save us all some trouble, and assume
+				// that the background color is indeed black for ANSI
+				// escape code consoles.
+				return new Color(0, 0, 0, 1);
             }
         }
 
@@ -32,7 +38,12 @@ namespace Flame.Front.Cli
         {
             get
             {
-                return DefaultConsole.DefaultForegroundColor;
+				// Mono seems to default to white as background color,
+				// no matter the settings. Let's just assume that the
+				// foreground color is gray instead, because that
+				// color scheme lets us print far more interesting 
+				// diagnostics.
+				return new Color(0.75, 0.75, 0.75, 1);
             }
         }
 
