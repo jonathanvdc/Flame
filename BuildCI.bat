dc3b97630bbf5d988c9542b4b5ec2ff68a9bb440
jonathanvdc <jonathan.vdc@outlook.com>
2016-05-18 18:52:42 +0200

    BuildCI.bat is a little more sophisticated now

--- BuildCI.bat
+++ BuildCI.bat
@@ -1,15 +1,24 @@
 
 SET dsc=%1
-SHIFT
 
-%dsc% Pixie/Pixie.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %*
-%dsc% Pixie/Pixie.Xml.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %*
-%dsc% Flame/Flame.dsc.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %*
-%dsc% Flame.Compiler/Flame.Compiler.dsc.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %*
-%dsc% Flame.Optimization/Flame.Optimization.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %*
-%dsc% Flame.Syntax/Flame.Syntax.dsc.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %*
-%dsc% Flame.Markdown/Flame.Markdown.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %*
-%dsc% Flame.DSharp/Flame.DSharp.dsc.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %*
+:loop
+      ::-------------------------- has argument ?
+      if ["%~1"]==[""] (
+        goto end
+      )
+      set args=%args% %1
+      shift
+      goto loop
+:end
+
+%dsc% Pixie/Pixie.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %args%
+%dsc% Pixie/Pixie.Xml.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %args%
+%dsc% Flame/Flame.dsc.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %args%
+%dsc% Flame.Compiler/Flame.Compiler.dsc.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %args%
+%dsc% Flame.Optimization/Flame.Optimization.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %args%
+%dsc% Flame.Syntax/Flame.Syntax.dsc.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %args%
+%dsc% Flame.Markdown/Flame.Markdown.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %args%
+%dsc% Flame.DSharp/Flame.DSharp.dsc.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %args%
 cd ./Flame.Cecil/
 msbuild /p:Configuration=Release Flame.Cecil.mono.sln
 cd ..
