afa041c9092869e2dd01322c4ea389022045e04b
jonathanvdc <jonathan.vdc@outlook.com>
2016-06-03 18:55:25 +0200

    CompilerName also prints the Flame version now

--- Flame.Front.Common/CompilerName.cs
+++ Flame.Front.Common/CompilerName.cs
@@ -17,12 +17,25 @@ namespace Flame.Front
     /// </summary>
     public class CompilerName
     {
-        public CompilerName(string Name, Version CurrentVersion, string Title, string ReleaseUrl)
+        public CompilerName(
+            string Name, Lazy<Version> CurrentVersion, 
+            string Title, string ReleaseUrl)
+            : this(
+                Name, CurrentVersion, Title, ReleaseUrl, 
+                new Lazy<IEnumerable<MarkupNode>>(
+                    () => Enumerable.Empty<MarkupNode>()))
+        { }
+
+        public CompilerName(
+            string Name, Lazy<Version> CurrentVersion, 
+            string Title, string ReleaseUrl,
+            Lazy<IEnumerable<MarkupNode>> ExtraVersionInfo)
         {
             this.Name = Name;
             this.CurrentVersion = CurrentVersion;
             this.Title = Title;
             this.ReleaseUrl = ReleaseUrl;
+            this.ExtraVersionInfo = ExtraVersionInfo;
         }
 
         /// <summary>
@@ -47,7 +60,13 @@ namespace Flame.Front
         /// Gets the compiler's version.
         /// </summary>
         /// <value>The compiler's version.</value>
-        public Version CurrentVersion { get; private set; }
+        public Lazy<Version> CurrentVersion { get; private set; }
+
+        /// <summary>
+        /// Gets formatted extra version information. 
+        /// Each node is printed on a separate line.
+        /// </summary>
+        public Lazy<IEnumerable<MarkupNode>> ExtraVersionInfo { get; private set; }
 
         /// <summary>
         /// Creates a compiler version for this compiler assembly.
@@ -56,32 +75,108 @@ namespace Flame.Front
         /// </summary>
         public static CompilerName Create(string Name, string Title, string ReleaseUrl)
         {
-            var version = Assembly.GetEntryAssembly().GetName().Version;
-            return new CompilerName(Name, version, Title, ReleaseUrl);
+            return Create(Name, Title, ReleaseUrl, new Lazy<IEnumerable<MarkupNode>>(
+                () => Enumerable.Empty<MarkupNode>()));
+        }
+
+        /// <summary>
+        /// Creates a compiler version for this compiler assembly.
+        /// The compiler version number is extracted from the assembly
+        /// metadata. Extra version information can also be provided.
+        /// </summary>
+        public static CompilerName Create(
+            string Name, string Title, string ReleaseUrl, 
+            Lazy<IEnumerable<MarkupNode>> ExtraVersionInfo)
+        {
+            var version = new Lazy<Version>(() => Assembly.GetEntryAssembly().GetName().Version);
+            return new CompilerName(Name, version, Title, ReleaseUrl, ExtraVersionInfo);
+        }
+
+        /// <summary>
+        /// Gets the version number of the Flame libraries. 
+        /// </summary>
+        /// <returns>
+        /// The version of Flame.Front is assumed to be
+        /// representative of all Flame libraries.
+        /// </returns>
+        public static Version GetFlameVersion()
+        {
+            return Assembly.GetExecutingAssembly().GetName().Version;
+        }
+
+        /// <summary>
+        /// Gets a sequence of markup nodes which represent formatted info
+        /// that pertains to the version number. 
+        /// Each node is to be printed on a separate line.
+        /// </summary>
+        public IEnumerable<MarkupNode> FormattedVersion
+        {
+            get
+            {
+                var nodes = new List<MarkupNode>();
+
+                nodes.Add(new MarkupNode("#group", new MarkupNode[]
+                {
+                    new MarkupNode(NodeConstants.BrightNodeType, Name),
+                    new MarkupNode(
+                        NodeConstants.TextNodeType, 
+                        " version " + CurrentVersion.Value.ToString(3) +
+                        " (based on Flame " + GetFlameVersion().ToString(3) + ")")
+                }));
+
+                nodes.AddRange(ExtraVersionInfo.Value);
+
+                return nodes;
+            }
+        }
+
+        /// <summary>
+        /// Gets a sequence of markup nodes which represent formatted info
+        /// that pertains to the version number, as well as some 
+        /// information about the current environment.
+        /// Each node is to be printed on a separate line.
+        /// </summary>
+        public IEnumerable<MarkupNode> FormattedInfo
+        {
+            get
+            {
+                var nodes = new List<MarkupNode>();
+
+                nodes.AddRange(FormattedVersion);
+                WriteVariable("Platform", ConsoleEnvironment.OSVersionString, nodes);
+                WriteVariable("Console", ConsoleEnvironment.TerminalIdentifier, nodes);
+                if (!string.IsNullOrWhiteSpace(ReleaseUrl))
+                    nodes.Add(new MarkupNode(NodeConstants.TextNodeType, "You can check for new releases at " + ReleaseUrl));
+                nodes.Add(new MarkupNode(NodeConstants.TextNodeType, "Thanks for using " + Title + "! Have fun writing code."));
+
+                return nodes;
+            }
+        }
+
+        /// <summary>
+        /// Prints each node in the given sequence of nodes
+        /// on a separate line.
+        /// </summary>
+        public static void PrintLines(
+            IEnumerable<MarkupNode> Lines, ConsoleLog Log)
+        {
+            foreach (var item in Lines)
+            {
+                Log.WriteEntry(new LogEntry("", item));
+            }
+        }
+
+        public void PrintVersion(ConsoleLog Log)
+        {
+            PrintLines(FormattedVersion, Log);
         }
 
         /// <summary>
         /// Prints this compiler version to the given log.
         /// </summary>
-        public void Print(ConsoleLog Log)
+        public void PrintInfo(ConsoleLog Log)
         {
-            var nodes = new List<MarkupNode>();
-
-            nodes.Add(new MarkupNode("#group", new MarkupNode[]
-            {
-                new MarkupNode(NodeConstants.BrightNodeType, Name),
-                new MarkupNode(NodeConstants.TextNodeType, " version " + CurrentVersion.ToString())
-            }));
-            WriteVariable("Platform", ConsoleEnvironment.OSVersionString, nodes);
-            WriteVariable("Console", ConsoleEnvironment.TerminalIdentifier, nodes);
-            if (!string.IsNullOrWhiteSpace(ReleaseUrl))
-                nodes.Add(new MarkupNode(NodeConstants.TextNodeType, "You can check for new releases at " + ReleaseUrl));
-            nodes.Add(new MarkupNode(NodeConstants.TextNodeType, "Thanks for using " + Title + "! Have fun writing code."));
-
-            foreach (var item in nodes)
-            {
-                Log.WriteEntry(new LogEntry("", item));
-            }
+            PrintLines(FormattedInfo, Log);
         }
 
         private static void WriteVariable(string Name, string Value, List<MarkupNode> Target)
@@ -92,28 +187,27 @@ namespace Flame.Front
             }
         }
 
-		/// <summary>
-		/// Prints information pertaining to the current color scheme.
-		/// </summary>
-		public static void PrintColorScheme(ConsoleLog Log)
-		{
-			var nodes = new List<MarkupNode>();
+        /// <summary>
+        /// Prints information pertaining to the current color scheme.
+        /// </summary>
+        public static void PrintColorScheme(ConsoleLog Log)
+        {
+            var nodes = new List<MarkupNode>();
+            nodes.Add(new MarkupNode("#group", new MarkupNode[]
+            {
+                new MarkupNode(NodeConstants.TextNodeType, "foreground color: "),
+                new MarkupNode(NodeConstants.TextNodeType, Log.ForegroundColor.ToString())
+            }));
+            nodes.Add(new MarkupNode("#group", new MarkupNode[]
+            {
+                new MarkupNode(NodeConstants.TextNodeType, "background color: "),
+                new MarkupNode(NodeConstants.TextNodeType, Log.BackgroundColor.ToString())
+            }));
 
-			nodes.Add(new MarkupNode("#group", new MarkupNode[]
-			{
-				new MarkupNode(NodeConstants.TextNodeType, "foreground color: "),
-				new MarkupNode(NodeConstants.TextNodeType, Log.ForegroundColor.ToString())
-			}));
-			nodes.Add(new MarkupNode("#group", new MarkupNode[]
-			{
-				new MarkupNode(NodeConstants.TextNodeType, "background color: "),
-				new MarkupNode(NodeConstants.TextNodeType, Log.BackgroundColor.ToString())
-			}));
-
-			foreach (var item in nodes)
-			{
-				Log.WriteEntry(new LogEntry("", item));
-			}
-		}
+            foreach (var item in nodes)
+            {
+                Log.WriteEntry(new LogEntry("", item));
+            }
+        }
     }
 }
