034765372f86e4f76332a1f390e9ae3de110c641
jonathanvdc <jonathan.vdc@outlook.com>
2016-07-27 21:21:42 +0200

    Added Flame.Build.Lazy, which originated from Flame.Ecs

---
+++ Flame.Build.Lazy/LazyDescribedAccessor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Flame.Build.Lazy
+{
+    public class LazyDescribedAccessor : LazyDescribedMethod, IAccessor
+    {
+        public LazyDescribedAccessor(
+            AccessorType AccessorType, IProperty DeclaringProperty, 
+            Action<LazyDescribedAccessor> AnalyzeBody)
+            : base(
+                new SimpleName(
+                    AccessorType.ToString().ToLower() + "_" 
+                    + DeclaringProperty.Name.ToString()), 
+                DeclaringProperty.DeclaringType, 
+                x => AnalyzeBody((LazyDescribedAccessor)x))
+        {
+            this.AccessorType = AccessorType;
+            this.DeclaringProperty = DeclaringProperty;
+        }
+
+        public AccessorType AccessorType { get; private set; }
+        public IProperty DeclaringProperty { get; private set; }
+    }
+}
+
