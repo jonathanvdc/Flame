aaca1a4678fbf3142ea5a65fd3f236d5de13fdc9
jonathanvdc <jonathan.vdc@outlook.com>
2016-07-28 13:08:26 +0200

    Added doc-comments to LazyDescribedAccessor

--- Flame.Build.Lazy/LazyDescribedAccessor.cs
+++ Flame.Build.Lazy/LazyDescribedAccessor.cs
@@ -2,24 +2,37 @@
 
 namespace Flame.Build.Lazy
 {
+    /// <summary>
+    /// An accessor implementation that constructs itself lazily.
+    /// </summary>
     public class LazyDescribedAccessor : LazyDescribedMethod, IAccessor
     {
+        /// <summary>
+        /// Creates a new lazily described method from the given accessor type,
+        /// the declaring property, and a deferred construction action.
+        /// </summary>
         public LazyDescribedAccessor(
-            AccessorType AccessorType, IProperty DeclaringProperty, 
+            AccessorType AccessorType, IProperty DeclaringProperty,
             Action<LazyDescribedAccessor> AnalyzeBody)
             : base(
                 new SimpleName(
-                    AccessorType.ToString().ToLower() + "_" 
-                    + DeclaringProperty.Name.ToString()), 
-                DeclaringProperty.DeclaringType, 
+                    AccessorType.ToString().ToLower() + "_"
+                    + DeclaringProperty.Name.ToString()),
+                DeclaringProperty.DeclaringType,
                 x => AnalyzeBody((LazyDescribedAccessor)x))
         {
             this.AccessorType = AccessorType;
             this.DeclaringProperty = DeclaringProperty;
         }
 
+        /// <summary>
+        /// Gets this accessor's type.
+        /// </summary>
         public AccessorType AccessorType { get; private set; }
+
+        /// <summary>
+        /// Gets this accessor's declaring property.
+        /// </summary>
         public IProperty DeclaringProperty { get; private set; }
     }
 }
-
