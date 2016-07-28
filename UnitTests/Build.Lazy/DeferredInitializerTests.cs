6141ca3728454f50b664b01e86286f1862af33b5
jonathanvdc <jonathan.vdc@outlook.com>
2016-07-28 18:27:26 +0200

    Added a DeferredInitializerTests class

--- UnitTests/Build.Lazy/DeferredInitializerTests.cs
+++ UnitTests/Build.Lazy/DeferredInitializerTests.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Loyc;
 using Loyc.MiniTest;
+using Flame;
+using Flame.Collections;
+using Flame.Build.Lazy;
 
 namespace UnitTests
 {
@@ -11,7 +16,47 @@ namespace UnitTests
         {
         }
 
+        [Test]
+        public void TestNoInit()
+        {
+            int x = 0;
+            var init = new DeferredInitializer<object>(_ => x = 3);
+            Assert.AreEqual(x, 0);
+            Assert.IsFalse(init.HasInitialized);
+        }
 
+        [Test]
+        public void TestSimpleInit()
+        {
+            int x = 0;
+            var init = new DeferredInitializer<object>(_ => x = 3);
+            Assert.IsFalse(init.HasInitialized);
+            init.Initialize(null);
+            Assert.AreEqual(x, 3);
+            Assert.IsTrue(init.HasInitialized);
+            x = 0;
+            init.Initialize(null);
+            Assert.AreEqual(x, 0);
+            Assert.IsTrue(init.HasInitialized);
+        }
+
+        [Test]
+        public void TestMultithreadedInit()
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                var integer = new Box<int>(0);
+                var init = new DeferredInitializer<Box<int>>(x => x.Value++);
+                Assert.IsFalse(init.HasInitialized);
+                var tasks = new List<Task>();
+                for (int i = 0; i < 20; i++)
+                {
+                    tasks.Add(Task.Run(() => init.Initialize(integer)));
+                }
+                Task.WaitAll(tasks.ToArray());
+                Assert.IsTrue(init.HasInitialized);
+                Assert.AreEqual(integer.Value, 1);
+            }
+        }
     }
 }
-
