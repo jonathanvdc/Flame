94174d205241c20af3391219fe4097089376510a
jonathanvdc <jonathan.vdc@outlook.com>
2016-07-28 18:04:52 +0200

    Created a UnitTests project

---
+++ UnitTests/Build.Lazy/DeferredInitializerTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Loyc;
+using Loyc.MiniTest;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class DeferredInitializerTests
+    {
+        public DeferredInitializerTests()
+        {
+        }
+
+
+    }
+}
+
