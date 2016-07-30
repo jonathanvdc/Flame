0a1911c6694c94423a2ee0466f0facfeafef1ec2
jonathanvdc <jonathan.vdc@outlook.com>
2016-07-30 15:14:49 +0200

    Fixed an assert compilation bug

---
+++ Flame.Cecil/Emit/VirtualPopBlock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flame.Compiler;
+
+namespace Flame.Cecil.Emit
+{
+    public class VirtualPopBlock : ICecilBlock
+    {
+        public VirtualPopBlock(ICecilBlock Value)
+        {
+            this.Value = Value;
+        }
+
+        public ICodeGenerator CodeGenerator { get { return Value.CodeGenerator; } }
+        public ICecilBlock Value { get; private set; }
+
+        public void Emit(IEmitContext Context)
+        {
+            Value.Emit(Context);
+            if (!Value.BlockType.Equals(PrimitiveTypes.Void))
+            {
+                Context.Stack.Pop();
+            }
+        }
+
+        public IType BlockType
+        {
+            get { return PrimitiveTypes.Void; }
+        }
+    }
+}
