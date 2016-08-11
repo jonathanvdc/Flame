e6f1e26a9d7b783978f9a435dfa4a7a08b399f15
jonathanvdc <jonathan.vdc@outlook.com>
2016-08-11 14:32:15 +0200

    Create IntegerValue class
    This class can later be extended to provide arbitrary-precision integer
    arithmetic. Eventually, IntegerValue may allow us to replace the current
    set of integer types and expressions by a more general infrastructure.

--- UnitTests/Program.cs
+++ UnitTests/Program.cs
@@ -72,7 +72,8 @@ namespace UnitTests
             return RunTests.RunMany(
                 new SetTests(),
                 new ShadowingTests(),
-                new TypeSystemTests());
+                new TypeSystemTests(),
+                new IntegerValueTests());
         }
 
         public static int Flame_Build_Lazy()
