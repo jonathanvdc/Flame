49fcf705579e4458e25eec8f08cd81f5c5f78a92
jonathanvdc <jonathan.vdc@outlook.com>
2016-11-27 21:29:34 +0100

    Move LazyDescribed{,Type}Member into separate files

---
+++ Flame.Build.Lazy/LazyDescribedMember.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Flame.Build;
+using System.Collections.Generic;
+using Flame.Compiler;
+using System.Threading;
+
+namespace Flame.Build.Lazy
+{
+    /// <summary>
+    /// A base class for members that are constructed lazily.
+    /// </summary>
+    public abstract class LazyDescribedMember : IMember
+    {
+        /// <summary>
+        /// Initializes this lazily described member with the given name.
+        /// </summary>
+        public LazyDescribedMember(UnqualifiedName Name)
+        {
+            this.Name = Name;
+            this.attributeList = new AttributeMapBuilder();
+        }
+
+        /// <summary>
+        /// Gets this member's unqualified name.
+        /// </summary>
+        public UnqualifiedName Name { get; private set; }
+
+        /// <summary>
+        /// Gets this member's qualified name.
+        /// </summary>
+        public abstract QualifiedName FullName { get; }
+
+        /// <summary>
+        /// Constructs the initial state of this lazily described member.
+        /// This method is called on-demand.
+        /// </summary>
+        protected abstract void CreateBody();
+
+        private AttributeMapBuilder attributeList;
+
+        /// <summary>
+        /// Adds the given attribute to this member's attribute map.
+        /// </summary>
+        public void AddAttribute(IAttribute Attribute)
+        {
+            CreateBody();
+            attributeList.Add(Attribute);
+        }
+
+        /// <summary>
+        /// Adds the given sequence of attributes to this member's attributes map.
+        /// </summary>
+        public void AddAttributes(IEnumerable<IAttribute> Attributes)
+        {
+            CreateBody();
+            attributeList.AddRange(Attributes);
+        }
+
+        /// <summary>
+        /// Adds the given attribute map to this member's attributes map.
+        /// </summary>
+        public void AddAttributes(AttributeMap Attributes)
+        {
+            CreateBody();
+            attributeList.AddRange(Attributes);
+        }
+
+        /// <summary>
+        /// Adds the given attribute map builder to this member's attributes map.
+        /// </summary>
+        public void AddAttributes(AttributeMapBuilder Attributes)
+        {
+            CreateBody();
+            attributeList.AddRange(Attributes);
+        }
+
+        /// <summary>
+        /// Removes all attributes of the given type from
+        /// the attribute map.
+        /// </summary>
+        /// <param name="AttributeType">
+        /// The attribute type whose instances will be removed
+        /// from this attribute map.
+        /// </param>
+        public void RemoveAttributes(IType AttributeType)
+        {
+            CreateBody();
+            attributeList.RemoveAll(AttributeType);
+        }
+
+        /// <summary>
+        /// Gets this member's attribute map.
+        /// </summary>
+        public AttributeMap Attributes
+        {
+            get
+            {
+                CreateBody();
+                return new AttributeMap(attributeList);
+            }
+        }
+
+        /// <summary>
+        /// Creates a string that identifies this member.
+        /// </summary>
+        public override string ToString()
+        {
+            return FullName.ToString();
+        }
+    }
+}
---
+++ Flame.Build.Lazy/LazyDescribedTypeMember.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Flame.Build;
+using System.Collections.Generic;
+using Flame.Compiler;
+using System.Threading;
+
+namespace Flame.Build.Lazy
+{
+    /// <summary>
+    /// A base class for type members that are constructed lazily.
+    /// </summary>
+    public abstract class LazyDescribedTypeMember : LazyDescribedMember, ITypeMember
+    {
+        /// <summary>
+        /// Initializes this lazily described member with the given name and
+        /// declaring type.
+        /// </summary>
+        public LazyDescribedTypeMember(UnqualifiedName Name, IType DeclaringType)
+            : base(Name)
+        {
+            this.DeclaringType = DeclaringType;
+        }
+
+        /// <summary>
+        /// Gets the type that declared this member.
+        /// </summary>
+        /// <value>The type that declared this member.</value>
+        public IType DeclaringType { get; private set; }
+
+        /// <summary>
+        /// Gets this member's qualified name.
+        /// </summary>
+        public sealed override QualifiedName FullName
+        {
+            get
+            {
+                if (DeclaringType == null)
+                    return Name.Qualify();
+                else
+                    return Name.Qualify(DeclaringType.FullName);
+            }
+        }
+
+        private bool isStaticVal;
+
+        /// <summary>
+        /// Gets or sets a boolean flag that indicates whether this type member
+        /// is static or not.
+        /// </summary>
+        public bool IsStatic
+        {
+            get
+            {
+                CreateBody();
+                return isStaticVal;
+            }
+            set
+            {
+                CreateBody();
+                isStaticVal = value;
+            }
+        }
+    }
+}
