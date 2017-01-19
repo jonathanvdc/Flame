6df7fa426bd25be6026ebbab8d5c6425386ec301
jonathanvdc <jonathan.vdc@outlook.com>
2017-01-19 15:20:24 +0100

    Upgrade CI/bootstrapping compiler version

--- BootstrapFlame.sh
+++ BootstrapFlame.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -L https://github.com/jonathanvdc/Flame/releases/download/v0.8.3/dsc.zip > dsc.zip
+curl -L https://github.com/jonathanvdc/Flame/releases/download/v0.9.3/dsc.zip > dsc.zip
 unzip -o dsc.zip -d bin_dsc
 nuget restore Flame.Cecil/Flame.Cecil.sln
 # Perform one build to check that Flame is buildable
