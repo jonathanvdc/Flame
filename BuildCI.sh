7fc4b99b46476637d00800d8b04fb3e1faa3f74d
jonathanvdc <jonathan.vdc@outlook.com>
2017-03-31 21:21:24 +0200

    Use -O3 optimizations for CI builds

--- BuildCI.sh
+++ BuildCI.sh
@@ -3,14 +3,14 @@ shopt -s expand_aliases
 
 alias dsc="mono $1"
 
-dsc Pixie/Pixie.dsproj -Wall -Wextra -pedantic -O2 -repeat-command -time -g --debug
-dsc Pixie/Pixie.Xml.dsproj -Wall -Wextra -pedantic -O2 -repeat-command -time -g --debug
-dsc Flame/Flame.dsc.dsproj -Wall -Wextra -pedantic -O2 -repeat-command -time -g --debug
-dsc Flame.Compiler/Flame.Compiler.dsc.dsproj -Wall -Wextra -pedantic -O2 -repeat-command -time -g --debug
-dsc Flame.Optimization/Flame.Optimization.dsproj -Wall -Wextra -pedantic -O2 -repeat-command -time -g --debug
-dsc Flame.Syntax/Flame.Syntax.dsc.dsproj -Wall -Wextra -pedantic -O2 -repeat-command -time -g --debug
-dsc Flame.Markdown/Flame.Markdown.dsproj -Wall -Wextra -pedantic -O2 -repeat-command -time -g --debug
-dsc Flame.DSharp/Flame.DSharp.dsc.dsproj -Wall -Wextra -pedantic -O2 -repeat-command -time -g --debug
+dsc Pixie/Pixie.dsproj -Wall -Wextra -pedantic $2 -repeat-command -time -g --debug
+dsc Pixie/Pixie.Xml.dsproj -Wall -Wextra -pedantic $2 -repeat-command -time -g --debug
+dsc Flame/Flame.dsc.dsproj -Wall -Wextra -pedantic $2 -repeat-command -time -g --debug
+dsc Flame.Compiler/Flame.Compiler.dsc.dsproj -Wall -Wextra -pedantic $2 -repeat-command -time -g --debug
+dsc Flame.Optimization/Flame.Optimization.dsproj -Wall -Wextra -pedantic $2 -repeat-command -time -g --debug
+dsc Flame.Syntax/Flame.Syntax.dsc.dsproj -Wall -Wextra -pedantic $2 -repeat-command -time -g --debug
+dsc Flame.Markdown/Flame.Markdown.dsproj -Wall -Wextra -pedantic $2 -repeat-command -time -g --debug
+dsc Flame.DSharp/Flame.DSharp.dsc.dsproj -Wall -Wextra -pedantic $2 -repeat-command -time -g --debug
 cd ./Flame.Cecil/
 xbuild /p:Configuration=Release Flame.Cecil.sln
 cd ..
