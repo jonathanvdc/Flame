e442bad3b1dc3983de5b959430a8a98db7243770
jonathanvdc <jonathan.vdc@outlook.com>
2017-04-01 21:05:26 +0200

    Update the BoostrapFlame.sh script

--- BootstrapFlame.sh
+++ BootstrapFlame.sh
@@ -3,8 +3,8 @@ curl -L https://github.com/jonathanvdc/Flame/releases/download/v0.9.3/dsc.zip > 
 unzip -o dsc.zip -d bin_dsc
 nuget restore Flame.Cecil/Flame.Cecil.sln
 # Perform one build to check that Flame is buildable
-./BuildCI.sh bin_dsc/dsc.exe
+./BuildCI.sh bin_dsc/dsc.exe -Og
 # Remove the version of dsc that we curled down.
 rm -rf dsc.zip bin_dsc
 # Perform a second build to make sure that Flame can bootstrap
-./BuildCI.sh dsc/bin/Release/dsc.exe
+./BuildCI.sh dsc/bin/Release/dsc.exe -O2
