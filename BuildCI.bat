d445109b9a0b68c55f1439ce2e1a1484beed785b
jonathanvdc <jonathan.vdc@outlook.com>
2017-04-01 13:12:02 +0200

    Update CI scripts to initially perform an -O1 build

--- BuildCI.bat
+++ BuildCI.bat
@@ -12,14 +12,14 @@ shift
       goto loop
 :end
 
-%dsc% Pixie/Pixie.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %args%
-%dsc% Pixie/Pixie.Xml.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %args%
-%dsc% Flame/Flame.dsc.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %args%
-%dsc% Flame.Compiler/Flame.Compiler.dsc.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %args%
-%dsc% Flame.Optimization/Flame.Optimization.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %args%
-%dsc% Flame.Syntax/Flame.Syntax.dsc.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %args%
-%dsc% Flame.Markdown/Flame.Markdown.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %args%
-%dsc% Flame.DSharp/Flame.DSharp.dsc.dsproj -Wall -Wextra -pedantic -O2 -time -g --debug -repeat-command %args%
+%dsc% Pixie/Pixie.dsproj -Wall -Wextra -pedantic -time -g --debug -repeat-command %args%
+%dsc% Pixie/Pixie.Xml.dsproj -Wall -Wextra -pedantic -time -g --debug -repeat-command %args%
+%dsc% Flame/Flame.dsc.dsproj -Wall -Wextra -pedantic -time -g --debug -repeat-command %args%
+%dsc% Flame.Compiler/Flame.Compiler.dsc.dsproj -Wall -Wextra -pedantic -time -g --debug -repeat-command %args%
+%dsc% Flame.Optimization/Flame.Optimization.dsproj -Wall -Wextra -pedantic -time -g --debug -repeat-command %args%
+%dsc% Flame.Syntax/Flame.Syntax.dsc.dsproj -Wall -Wextra -pedantic -time -g --debug -repeat-command %args%
+%dsc% Flame.Markdown/Flame.Markdown.dsproj -Wall -Wextra -pedantic -time -g --debug -repeat-command %args%
+%dsc% Flame.DSharp/Flame.DSharp.dsc.dsproj -Wall -Wextra -pedantic -time -g --debug -repeat-command %args%
 cd ./Flame.Cecil/
 msbuild /p:Configuration=Release /verbosity:minimal Flame.Cecil.sln
 cd ..
