34140eea31aa9949c51073178f233ddb1443860c
jonathanvdc <jonathan.vdc@outlook.com>
2017-04-26 10:44:12 +0200

    Untabify AnsiConsoleStyle.cs

--- Flame.Front/Cli/AnsiConsoleStyle.cs
+++ Flame.Front/Cli/AnsiConsoleStyle.cs
@@ -41,33 +41,33 @@ namespace Flame.Front.Cli
             Console.Write(GetEscapeSequence(OldStyle, InitialStyle));
         }
 
-		private static readonly Dictionary<ConsoleColor, int> colorIdents = new Dictionary<ConsoleColor, int>()
-		{
-			{ ConsoleColor.Black, 0 },
-			{ ConsoleColor.DarkRed, 1 },
-			{ ConsoleColor.DarkGreen, 2 },
-			{ ConsoleColor.DarkYellow, 3 },
-			{ ConsoleColor.DarkBlue, 4 },
-			{ ConsoleColor.DarkMagenta, 5 },
-			{ ConsoleColor.DarkCyan, 6 },
-			{ ConsoleColor.Gray, 7 },
-			{ ConsoleColor.DarkGray, 0 },
-			{ ConsoleColor.Red, 1 },
-			{ ConsoleColor.Green, 2 },
-			{ ConsoleColor.Yellow, 3 },
-			{ ConsoleColor.Blue, 4 },
-			{ ConsoleColor.Magenta, 5 },
-			{ ConsoleColor.Cyan, 6 },
-			{ ConsoleColor.White, 7 }
-		};
+        private static readonly Dictionary<ConsoleColor, int> colorIdents = new Dictionary<ConsoleColor, int>()
+        {
+            { ConsoleColor.Black, 0 },
+            { ConsoleColor.DarkRed, 1 },
+            { ConsoleColor.DarkGreen, 2 },
+            { ConsoleColor.DarkYellow, 3 },
+            { ConsoleColor.DarkBlue, 4 },
+            { ConsoleColor.DarkMagenta, 5 },
+            { ConsoleColor.DarkCyan, 6 },
+            { ConsoleColor.Gray, 7 },
+            { ConsoleColor.DarkGray, 0 },
+            { ConsoleColor.Red, 1 },
+            { ConsoleColor.Green, 2 },
+            { ConsoleColor.Yellow, 3 },
+            { ConsoleColor.Blue, 4 },
+            { ConsoleColor.Magenta, 5 },
+            { ConsoleColor.Cyan, 6 },
+            { ConsoleColor.White, 7 }
+        };
 
-		private static readonly HashSet<ConsoleColor> boldColors = new HashSet<ConsoleColor>()
-		{
-			ConsoleColor.DarkGray, ConsoleColor.Red,
-			ConsoleColor.Green, ConsoleColor.Yellow,
-			ConsoleColor.Blue, ConsoleColor.Magenta,
-			ConsoleColor.Cyan, ConsoleColor.White
-		};
+        private static readonly HashSet<ConsoleColor> boldColors = new HashSet<ConsoleColor>()
+        {
+            ConsoleColor.DarkGray, ConsoleColor.Red,
+            ConsoleColor.Green, ConsoleColor.Yellow,
+            ConsoleColor.Blue, ConsoleColor.Magenta,
+            ConsoleColor.Cyan, ConsoleColor.White
+        };
 
         public string GetEscapeSequence(AnsiConsoleStyle OldStyle, AnsiConsoleStyle InitialStyle)
         {
@@ -88,17 +88,17 @@ namespace Flame.Front.Cli
             }
             if (!ResetAll)
             {
-				int oldFgIdent = colorIdents[OldStyle.ForegroundConsoleColor];
-				int oldBgIdent = colorIdents[OldStyle.BackgroundConsoleColor];
-				int newFgIdent = colorIdents[ForegroundConsoleColor];
-				int newBgIdent = colorIdents[BackgroundConsoleColor];
-				if (reset && (newFgIdent != colorIdents[InitialStyle.ForegroundConsoleColor]) || newFgIdent != oldFgIdent)
+                int oldFgIdent = colorIdents[OldStyle.ForegroundConsoleColor];
+                int oldBgIdent = colorIdents[OldStyle.BackgroundConsoleColor];
+                int newFgIdent = colorIdents[ForegroundConsoleColor];
+                int newBgIdent = colorIdents[BackgroundConsoleColor];
+                if (reset && (newFgIdent != colorIdents[InitialStyle.ForegroundConsoleColor]) || newFgIdent != oldFgIdent)
                 {
-					codes.Add("3" + newFgIdent.ToString(CultureInfo.InvariantCulture));
+                    codes.Add("3" + newFgIdent.ToString(CultureInfo.InvariantCulture));
                 }
-				if (reset && (newBgIdent != colorIdents[InitialStyle.BackgroundConsoleColor]) || newBgIdent != oldBgIdent)
+                if (reset && (newBgIdent != colorIdents[InitialStyle.BackgroundConsoleColor]) || newBgIdent != oldBgIdent)
                 {
-					codes.Add("4" + newBgIdent.ToString(CultureInfo.InvariantCulture));
+                    codes.Add("4" + newBgIdent.ToString(CultureInfo.InvariantCulture));
                 }
             }
 
