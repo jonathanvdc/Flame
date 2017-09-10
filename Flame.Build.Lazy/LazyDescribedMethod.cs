deb8bf3e0158f626d0536b9f582d43688cfcd9bd
jonathanvdc <jonathan.vdc@outlook.com>
2017-09-10 15:02:30 +0200

    Untabify Flame.Build.Lazy

--- Flame.Build.Lazy/LazyDescribedMethod.cs
+++ Flame.Build.Lazy/LazyDescribedMethod.cs
@@ -9,27 +9,27 @@ namespace Flame.Build.Lazy
     /// <summary>
     /// A method implementation that constructs itself lazily.
     /// </summary>
-	public class LazyDescribedMethod : LazyDescribedTypeMember, IBodyMethod
-	{
+    public class LazyDescribedMethod : LazyDescribedTypeMember, IBodyMethod
+    {
         /// <summary>
         /// Creates a new lazily described method from the given name,
         /// declaring type, and a deferred construction action.
         /// </summary>
-		public LazyDescribedMethod(
+        public LazyDescribedMethod(
             UnqualifiedName Name, IType DeclaringType,
             Action<LazyDescribedMethod> AnalyzeHeader,
             Action<LazyDescribedMethod> AnalyzeBaseMethods,
             Action<LazyDescribedMethod> AnalyzeBody)
-			: base(Name, DeclaringType)
-		{
-			this.baseMethods = new List<IMethod>();
+            : base(Name, DeclaringType)
+        {
+            this.baseMethods = new List<IMethod>();
             this.parameters = new List<IParameter>();
             this.baseMethods = new List<IMethod>();
             this.genericParams = new List<IGenericParameter>();
             this.analyzeHeader = new DeferredInitializer<LazyDescribedMethod>(AnalyzeHeader);
             this.analyzeBaseMethods = new DeferredInitializer<LazyDescribedMethod>(AnalyzeBaseMethods);
             this.analyzeBody = new DeferredInitializer<LazyDescribedMethod>(AnalyzeBody);
-		}
+        }
 
         /// <summary>
         /// Creates a new lazily described method from the given name,
@@ -49,152 +49,152 @@ namespace Flame.Build.Lazy
             this.analyzeBody = new DeferredInitializer<LazyDescribedMethod>(x => analyzeHeader.Initialize(x));
         }
 
-		private DeferredInitializer<LazyDescribedMethod> analyzeHeader;
+        private DeferredInitializer<LazyDescribedMethod> analyzeHeader;
         private DeferredInitializer<LazyDescribedMethod> analyzeBaseMethods;
         private DeferredInitializer<LazyDescribedMethod> analyzeBody;
 
-		private IType retType;
+        private IType retType;
 
         /// <summary>
         /// Gets or sets this method's return type.
         /// </summary>
-		public IType ReturnType
-		{
-			get
-			{
-				CreateBody();
-				return retType;
-			}
-			set
-			{
-				CreateBody();
-				retType = value;
-			}
-		}
+        public IType ReturnType
+        {
+            get
+            {
+                CreateBody();
+                return retType;
+            }
+            set
+            {
+                CreateBody();
+                retType = value;
+            }
+        }
 
-		private bool isCtorVal;
+        private bool isCtorVal;
 
         /// <summary>
         /// Gets or sets a boolean flag that tells if this method is a constructor.
         /// </summary>
-		public bool IsConstructor
-		{
-			get
-			{
-				CreateBody();
-				return isCtorVal;
-			}
-			set
-			{
-				CreateBody();
-				isCtorVal = value;
-			}
-		}
+        public bool IsConstructor
+        {
+            get
+            {
+                CreateBody();
+                return isCtorVal;
+            }
+            set
+            {
+                CreateBody();
+                isCtorVal = value;
+            }
+        }
 
-		private IStatement bodyStmt;
+        private IStatement bodyStmt;
 
         /// <summary>
         /// Gets or sets this method's body statement.
         /// </summary>
-		public IStatement Body
-		{
-			get
-			{
+        public IStatement Body
+        {
+            get
+            {
                 analyzeBody.Initialize(this);
-				return bodyStmt;
-			}
-			set
-			{
+                return bodyStmt;
+            }
+            set
+            {
                 analyzeBody.Initialize(this);
-				bodyStmt = value;
-			}
-		}
+                bodyStmt = value;
+            }
+        }
 
-		/// <summary>
-		/// Gets the method's body statement.
-		/// </summary>
-		public IStatement GetMethodBody()
-		{
-			return Body;
-		}
+        /// <summary>
+        /// Gets the method's body statement.
+        /// </summary>
+        public IStatement GetMethodBody()
+        {
+            return Body;
+        }
 
         /// <summary>
         /// Constructs the initial state of this lazily described member.
         /// This method is called on-demand.
         /// </summary>
-		protected override void CreateBody()
-		{
+        protected override void CreateBody()
+        {
             analyzeHeader.Initialize(this);
-		}
+        }
 
-		private List<IParameter> parameters;
+        private List<IParameter> parameters;
 
         /// <summary>
         /// Adds the given parameter to this method's parameter list.
         /// </summary>
-		public virtual void AddParameter(IParameter Parameter)
-		{
+        public virtual void AddParameter(IParameter Parameter)
+        {
             CreateBody();
-			parameters.Add(Parameter);
-		}
+            parameters.Add(Parameter);
+        }
 
         /// <summary>
         /// Gets this method's parameter list.
         /// </summary>
-		public IEnumerable<IParameter> Parameters
-		{
-			get
-			{
-				CreateBody();
-				return parameters;
-			}
-		}
+        public IEnumerable<IParameter> Parameters
+        {
+            get
+            {
+                CreateBody();
+                return parameters;
+            }
+        }
 
-		private List<IMethod> baseMethods;
+        private List<IMethod> baseMethods;
 
         /// <summary>
         /// Makes the given method a base method of this method.
         /// </summary>
-		public virtual void AddBaseMethod(IMethod Method)
-		{
+        public virtual void AddBaseMethod(IMethod Method)
+        {
             analyzeBaseMethods.Initialize(this);
-			baseMethods.Add(Method);
-		}
+            baseMethods.Add(Method);
+        }
 
         /// <summary>
         /// Gets the list of all base methods for this method.
         /// </summary>
-		public IEnumerable<IMethod> BaseMethods
-		{
-			get
-			{
+        public IEnumerable<IMethod> BaseMethods
+        {
+            get
+            {
                 analyzeBaseMethods.Initialize(this);
-				return baseMethods.ToArray();
-			}
-		}
+                return baseMethods.ToArray();
+            }
+        }
 
-		private List<IGenericParameter> genericParams;
+        private List<IGenericParameter> genericParams;
 
         /// <summary>
         /// Adds the given generic parameter to this method's generic parameter
         /// list.
         /// </summary>
-		public virtual void AddGenericParameter(IGenericParameter Parameter)
-		{
+        public virtual void AddGenericParameter(IGenericParameter Parameter)
+        {
             CreateBody();
-			genericParams.Add(Parameter);
-		}
+            genericParams.Add(Parameter);
+        }
 
-		/// <summary>
-		/// Gets this method's list of generic parameters.
-		/// </summary>
-		public IEnumerable<IGenericParameter> GenericParameters
-		{
-			get
-			{
-				CreateBody();
-				return genericParams;
-			}
-		}
-	}
+        /// <summary>
+        /// Gets this method's list of generic parameters.
+        /// </summary>
+        public IEnumerable<IGenericParameter> GenericParameters
+        {
+            get
+            {
+                CreateBody();
+                return genericParams;
+            }
+        }
+    }
 }
--- Flame.Build.Lazy/LazyDescribedType.cs
+++ Flame.Build.Lazy/LazyDescribedType.cs
@@ -9,56 +9,56 @@ namespace Flame.Build.Lazy
     /// A type implementation that constructs itself lazily.
     /// </summary>
     public sealed class LazyDescribedType : LazyDescribedMember, IType, INamespace
-	{
+    {
         /// <summary>
         /// Creates a new lazily described type from the given name,
         /// declaring namespace, and a deferred construction action.
         /// </summary>
-		public LazyDescribedType(
-			UnqualifiedName Name, INamespace Namespace,
-			Action<LazyDescribedType> AnalyzeBody)
-			: base(Name)
-		{
-			this.DeclaringNamespace = Namespace;
+        public LazyDescribedType(
+            UnqualifiedName Name, INamespace Namespace,
+            Action<LazyDescribedType> AnalyzeBody)
+            : base(Name)
+        {
+            this.DeclaringNamespace = Namespace;
             this.analyzeBody = new DeferredInitializer<LazyDescribedType>(AnalyzeBody);
-			this.baseTypes = new List<IType>();
+            this.baseTypes = new List<IType>();
             this.methods = new List<IMethod>();
             this.fields = new List<IField>();
             this.properties = new List<IProperty>();
             this.nestedTypes = new List<IType>();
             this.typeParams = new List<IGenericParameter>();
-		}
+        }
 
-		private List<IType> baseTypes;
-		private List<IMethod> methods;
-		private List<IField> fields;
-		private List<IProperty> properties;
-		private List<IType> nestedTypes;
-		private List<IGenericParameter> typeParams;
+        private List<IType> baseTypes;
+        private List<IMethod> methods;
+        private List<IField> fields;
+        private List<IProperty> properties;
+        private List<IType> nestedTypes;
+        private List<IGenericParameter> typeParams;
 
-		private DeferredInitializer<LazyDescribedType> analyzeBody;
+        private DeferredInitializer<LazyDescribedType> analyzeBody;
 
-		/// <summary>
-		/// Gets the declaring namespace for this type.
-		/// </summary>
-		/// <value>The declaring namespace.</value>
-		public INamespace DeclaringNamespace { get; private set; }
+        /// <summary>
+        /// Gets the declaring namespace for this type.
+        /// </summary>
+        /// <value>The declaring namespace.</value>
+        public INamespace DeclaringNamespace { get; private set; }
 
         /// <summary>
         /// Gets this type's qualified name.
         /// </summary>
-		public override QualifiedName FullName
-		{
-			get
-			{
-				return Name.Qualify(DeclaringNamespace.FullName);
-			}
-		}
+        public override QualifiedName FullName
+        {
+            get
+            {
+                return Name.Qualify(DeclaringNamespace.FullName);
+            }
+        }
 
         /// <summary>
         /// Gets this type's declaring assembly.
         /// </summary>
-		public IAssembly DeclaringAssembly
+        public IAssembly DeclaringAssembly
         {
             get { return DeclaringNamespace.DeclaringAssembly; }
         }
@@ -66,7 +66,7 @@ namespace Flame.Build.Lazy
         /// <summary>
         /// Gets this type's set of ancestry rules.
         /// </summary>
-		public IAncestryRules AncestryRules
+        public IAncestryRules AncestryRules
         {
             get { return DefinitionAncestryRules.Instance; }
         }
@@ -74,146 +74,146 @@ namespace Flame.Build.Lazy
         /// <summary>
         /// Gets this lazily described type's default value.
         /// </summary>
-		public IBoundObject GetDefaultValue()
-		{
-			return null;
-		}
+        public IBoundObject GetDefaultValue()
+        {
+            return null;
+        }
 
         /// <summary>
         /// Constructs the initial state of this lazily described member.
         /// This method is called on-demand.
         /// </summary>
-		protected override void CreateBody()
-		{
+        protected override void CreateBody()
+        {
             analyzeBody.Initialize(this);
-		}
+        }
 
-		/// <summary>
-		/// Gets this described type's set of base types.
-		/// </summary>
-		public IEnumerable<IType> BaseTypes
-		{
-			get
-			{
-				CreateBody();
-				return baseTypes;
-			}
-		}
+        /// <summary>
+        /// Gets this described type's set of base types.
+        /// </summary>
+        public IEnumerable<IType> BaseTypes
+        {
+            get
+            {
+                CreateBody();
+                return baseTypes;
+            }
+        }
 
-		/// <summary>
-		/// Adds the given type to this described type's set of base types.
-		/// </summary>
-		public void AddBaseType(IType BaseType)
-		{
+        /// <summary>
+        /// Adds the given type to this described type's set of base types.
+        /// </summary>
+        public void AddBaseType(IType BaseType)
+        {
             CreateBody();
-			this.baseTypes.Add(BaseType);
-		}
+            this.baseTypes.Add(BaseType);
+        }
 
-		/// <summary>
-		/// Gets all methods stored in this described type.
-		/// </summary>
-		public IEnumerable<IMethod> Methods
-		{
-			get
-			{
-				CreateBody();
-				return methods;
-			}
-		}
+        /// <summary>
+        /// Gets all methods stored in this described type.
+        /// </summary>
+        public IEnumerable<IMethod> Methods
+        {
+            get
+            {
+                CreateBody();
+                return methods;
+            }
+        }
 
-		/// <summary>
-		/// Adds the given method to this described type's list of methods.
-		/// </summary>
-		public void AddMethod(IMethod Method)
-		{
+        /// <summary>
+        /// Adds the given method to this described type's list of methods.
+        /// </summary>
+        public void AddMethod(IMethod Method)
+        {
             CreateBody();
-			this.methods.Add(Method);
-		}
+            this.methods.Add(Method);
+        }
 
-		/// <summary>
-		/// Gets all properties stored in this described type.
-		/// </summary>
-		public IEnumerable<IProperty> Properties
-		{
-			get
-			{
-				CreateBody();
-				return properties;
-			}
-		}
+        /// <summary>
+        /// Gets all properties stored in this described type.
+        /// </summary>
+        public IEnumerable<IProperty> Properties
+        {
+            get
+            {
+                CreateBody();
+                return properties;
+            }
+        }
 
-		/// <summary>
-		/// Adds the given property to this described type's list of properties.
-		/// </summary>
-		public void AddProperty(IProperty Property)
-		{
+        /// <summary>
+        /// Adds the given property to this described type's list of properties.
+        /// </summary>
+        public void AddProperty(IProperty Property)
+        {
             CreateBody();
-			this.properties.Add(Property);
-		}
+            this.properties.Add(Property);
+        }
 
-		/// <summary>
-		/// Gets all fields stored in this described type.
-		/// </summary>
-		public IEnumerable<IField> Fields
-		{
-			get
-			{
-				CreateBody();
-				return fields;
-			}
-		}
+        /// <summary>
+        /// Gets all fields stored in this described type.
+        /// </summary>
+        public IEnumerable<IField> Fields
+        {
+            get
+            {
+                CreateBody();
+                return fields;
+            }
+        }
 
-		/// <summary>
-		/// Adds the given field to this described type's list of fields.
-		/// </summary>
-		public void AddField(IField Field)
-		{
+        /// <summary>
+        /// Adds the given field to this described type's list of fields.
+        /// </summary>
+        public void AddField(IField Field)
+        {
             CreateBody();
-			this.fields.Add(Field);
-		}
+            this.fields.Add(Field);
+        }
 
-		/// <summary>
-		/// Gets all nested types stored in this described type.
-		/// </summary>
-		public IEnumerable<IType> Types
-		{
-			get
-			{
-				CreateBody();
-				return this.nestedTypes;
-			}
-		}
+        /// <summary>
+        /// Gets all nested types stored in this described type.
+        /// </summary>
+        public IEnumerable<IType> Types
+        {
+            get
+            {
+                CreateBody();
+                return this.nestedTypes;
+            }
+        }
 
-		/// <summary>
-		/// Adds the given type to this described type's list of nested
-		/// types.
-		/// </summary>
-		public void AddNestedType(IType Type)
-		{
+        /// <summary>
+        /// Adds the given type to this described type's list of nested
+        /// types.
+        /// </summary>
+        public void AddNestedType(IType Type)
+        {
             CreateBody();
-			this.nestedTypes.Add(Type);
-		}
+            this.nestedTypes.Add(Type);
+        }
 
-		/// <summary>
-		/// Adds the given generic parameter to this described type's
-		/// type parameter list.
-		/// </summary>
-		public void AddGenericParameter(IGenericParameter Value)
-		{
+        /// <summary>
+        /// Adds the given generic parameter to this described type's
+        /// type parameter list.
+        /// </summary>
+        public void AddGenericParameter(IGenericParameter Value)
+        {
             CreateBody();
-			this.typeParams.Add(Value);
-		}
+            this.typeParams.Add(Value);
+        }
 
         /// <summary>
         /// Gets this type's list of generic parameters.
         /// </summary>
-		public IEnumerable<IGenericParameter> GenericParameters
-		{
-			get
-			{
-				CreateBody();
-				return this.typeParams;
-			}
-		}
-	}
+        public IEnumerable<IGenericParameter> GenericParameters
+        {
+            get
+            {
+                CreateBody();
+                return this.typeParams;
+            }
+        }
+    }
 }
