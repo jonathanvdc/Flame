1b1ef906e26c68f36e490bf924e2630409a1c945
jonathanvdc <jonathan.vdc@outlook.com>
2018-02-06 14:53:56 +0100

    Define DescribedAssembly

---
+++ Flame/TypeSystem/DescribedAssembly.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+
+namespace Flame.TypeSystem
+{
+    /// <summary>
+    /// An assembly that can be constructed in piece by piece,
+    /// an imperative fashion.
+    /// </summary>
+    public sealed class DescribedAssembly : DescribedMember
+    {
+        /// <summary>
+        /// Creates an empty assembly with a particular name.
+        /// </summary>
+        /// <param name="fullName">The assembly's name.</param>
+        public DescribedAssembly(QualifiedName fullName)
+            : base(fullName)
+        {
+            this.definedTypes = new List<IType>();
+        }
+
+        private List<IType> definedTypes;
+
+        /// <inheritdoc/>
+        public IReadOnlyList<IType> Types => definedTypes;
+
+        /// <summary>
+        /// Adds a type to this assembly's list of defined types.
+        /// </summary>
+        /// <param name="type">The type to add.</param>
+        public void AddType(IType type)
+        {
+            definedTypes.Add(type);
+        }
+    }
+}
