77cb1421eb817a717ec21fbc10b4a3347251dddb
jonathanvdc <jonathan.vdc@outlook.com>
2018-02-08 12:48:42 +0100

    Define AllocaPrototype, AllocaArrayPrototype

---
+++ Flame.Compiler/Instructions/AllocaArrayPrototype.cs
@@ -0,0 +1,67 @@
+using System.Collections.Generic;
+using Flame.Collections;
+using Flame.TypeSystem;
+
+namespace Flame.Compiler.Instructions
+{
+    /// <summary>
+    /// A prototype for alloca-array instructions, which allocate an array
+    /// of values on the stack.
+    /// </summary>
+    public sealed class AllocaArrayPrototype : InstructionPrototype
+    {
+        private AllocaArrayPrototype(IType elementType)
+        {
+            this.ElementType = elementType;
+        }
+
+        /// <summary>
+        /// Gets the type of element to allocate storage for.
+        /// </summary>
+        /// <returns>The type of element.</returns>
+        public IType ElementType { get; private set; }
+
+        /// <inheritdoc/>
+        public override IType ResultType => ElementType.MakePointerType(PointerKind.Transient);
+
+        /// <inheritdoc/>
+        public override int ParameterCount => 1;
+
+        /// <inheritdoc/>
+        public override ExceptionSpecification ExceptionSpecification
+            => ExceptionSpecification.NoThrow;
+
+        /// <inheritdoc/>
+        public override IReadOnlyList<string> CheckConformance(Instruction instance, MethodBody body)
+        {
+            return EmptyArray<string>.Value;
+        }
+        private static readonly InterningCache<AllocaArrayPrototype> instanceCache
+            = new InterningCache<AllocaArrayPrototype>(
+                new StructuralAllocaArrayPrototypeComparer());
+
+        /// <summary>
+        /// Gets the alloca instruction prototype for a particular result type.
+        /// </summary>
+        /// <param name="resultType">The result type.</param>
+        /// <returns>A alloca instruction prototype.</returns>
+        public static AllocaArrayPrototype Create(IType resultType)
+        {
+            return instanceCache.Intern(new AllocaArrayPrototype(resultType));
+        }
+    }
+
+    internal sealed class StructuralAllocaArrayPrototypeComparer
+        : IEqualityComparer<AllocaArrayPrototype>
+    {
+        public bool Equals(AllocaArrayPrototype x, AllocaArrayPrototype y)
+        {
+            return x.ElementType.Equals(y.ElementType);
+        }
+
+        public int GetHashCode(AllocaArrayPrototype obj)
+        {
+            return obj.ElementType.GetHashCode();
+        }
+    }
+}
---
+++ Flame.Compiler/Instructions/AllocaPrototype.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using Flame.Collections;
+using Flame.TypeSystem;
+
+namespace Flame.Compiler.Instructions
+{
+    /// <summary>
+    /// A prototype for alloca instructions, which allocate a single value
+    /// on the stack.
+    /// </summary>
+    public sealed class AllocaPrototype : InstructionPrototype
+    {
+        private AllocaPrototype(IType elementType)
+        {
+            this.ElementType = elementType;
+        }
+
+        /// <summary>
+        /// Gets the type of element to allocate storage for.
+        /// </summary>
+        /// <returns>The type of element.</returns>
+        public IType ElementType { get; private set; }
+
+        /// <inheritdoc/>
+        public override IType ResultType => ElementType.MakePointerType(PointerKind.Reference);
+
+        /// <inheritdoc/>
+        public override int ParameterCount => 0;
+
+        /// <inheritdoc/>
+        public override ExceptionSpecification ExceptionSpecification
+            => ExceptionSpecification.NoThrow;
+
+        /// <inheritdoc/>
+        public override IReadOnlyList<string> CheckConformance(Instruction instance, MethodBody body)
+        {
+            return EmptyArray<string>.Value;
+        }
+
+        private static readonly InterningCache<AllocaPrototype> instanceCache
+            = new InterningCache<AllocaPrototype>(
+                new StructuralAllocaPrototypeComparer());
+
+        /// <summary>
+        /// Gets the alloca instruction prototype for a particular result type.
+        /// </summary>
+        /// <param name="resultType">The result type.</param>
+        /// <returns>A alloca instruction prototype.</returns>
+        public static AllocaPrototype Create(IType resultType)
+        {
+            return instanceCache.Intern(new AllocaPrototype(resultType));
+        }
+    }
+
+    internal sealed class StructuralAllocaPrototypeComparer
+        : IEqualityComparer<AllocaPrototype>
+    {
+        public bool Equals(AllocaPrototype x, AllocaPrototype y)
+        {
+            return x.ElementType.Equals(y.ElementType);
+        }
+
+        public int GetHashCode(AllocaPrototype obj)
+        {
+            return obj.ElementType.GetHashCode();
+        }
+    }
+}
