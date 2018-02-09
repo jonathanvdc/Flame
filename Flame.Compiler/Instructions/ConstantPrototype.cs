717e0750d77990ccc5d9292f5d3d4a123ff506cb
jonathanvdc <jonathan.vdc@outlook.com>
2018-02-09 14:41:42 +0100

    Define ConstantPrototype

---
+++ Flame.Compiler/Instructions/ConstantPrototype.cs
@@ -0,0 +1,72 @@
+using System.Collections.Generic;
+using Flame.Collections;
+
+namespace Flame.Compiler.Instructions
+{
+    /// <summary>
+    /// A prototype for instructions that produce a constant value.
+    /// </summary>
+    public sealed class ConstantPrototype : InstructionPrototype
+    {
+        private ConstantPrototype(Constant value, IType type)
+        {
+            this.Value = value;
+            this.type = type;
+        }
+
+        private IType type;
+
+        /// <summary>
+        /// Gets the constant value produced by instances of this
+        /// prototype.
+        /// </summary>
+        /// <returns>A constant value.</returns>
+        public Constant Value { get; private set; }
+
+        /// <inheritdoc/>
+        public override IType ResultType => type;
+
+        /// <inheritdoc/>
+        public override int ParameterCount => 0;
+
+        /// <inheritdoc/>
+        public override ExceptionSpecification ExceptionSpecification
+            => ExceptionSpecification.NoThrow;
+
+        /// <inheritdoc/>
+        public override IReadOnlyList<string> CheckConformance(Instruction instance, MethodBody body)
+        {
+            return EmptyArray<string>.Value;
+        }
+
+        private static readonly InterningCache<ConstantPrototype> instanceCache
+            = new InterningCache<ConstantPrototype>(
+                new StructuralConstantPrototypeComparer());
+
+        /// <summary>
+        /// Gets or creates a constant instruction prototype.
+        /// </summary>
+        /// <param name="value">The constant value to produce.</param>
+        /// <param name="type">The result type of prototype instances.</param>
+        /// <returns>A constant instruction prototype.</returns>
+        public static ConstantPrototype Create(Constant value, IType type)
+        {
+            return instanceCache.Intern(new ConstantPrototype(value, type));
+        }
+    }
+
+    internal sealed class StructuralConstantPrototypeComparer
+        : IEqualityComparer<ConstantPrototype>
+    {
+        public bool Equals(ConstantPrototype x, ConstantPrototype y)
+        {
+            return x.Value.Equals(y.Value)
+                && object.Equals(x.ResultType, y.ResultType);
+        }
+
+        public int GetHashCode(ConstantPrototype obj)
+        {
+            return (obj.Value.GetHashCode() << 3) ^ obj.ResultType.GetHashCode();
+        }
+    }
+}
