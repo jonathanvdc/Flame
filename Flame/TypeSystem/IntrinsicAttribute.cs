af5b12f69790c398fe854b81bfde74e7bc3e6abd
jonathanvdc <jonathan.vdc@outlook.com>
2018-02-09 15:24:43 +0100

    Define IntrinsicAttribute

---
+++ Flame/TypeSystem/IntrinsicAttribute.cs
@@ -0,0 +1,57 @@
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Flame.Collections;
+
+namespace Flame.TypeSystem
+{
+    /// <summary>
+    /// Describes a named attribute that is well-understood by the compiler.
+    /// </summary>
+    public sealed class IntrinsicAttribute : IAttribute
+    {
+        /// <summary>
+        /// Creates an intrinsic attribute with the given name and an empty argument list.
+        /// </summary>
+        /// <param name="name">The attribute's name.</param>
+        public IntrinsicAttribute(string name)
+            : this(name, EmptyArray<Constant>.Value)
+        { }
+
+        /// <summary>
+        /// Creates an intrinsic attribute with the given name and argument list.
+        /// </summary>
+        /// <param name="name">The attribute's name.</param>
+        /// <param name="arguments">The attribute's list of arguments.</param>
+        public IntrinsicAttribute(string name, IReadOnlyList<Constant> arguments)
+        {
+            this.Name = name;
+            this.Arguments = arguments;
+            this.AttributeType = attrTypes.GetOrAdd(name, SynthesizeAttributeType);
+        }
+
+        /// <summary>
+        /// Gets the name of this intrinsic attribute.
+        /// </summary>
+        /// <returns>The name of the intrinsic attribute.</returns>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Gets the list of arguments that is supplied to this intrinsic attribute.
+        /// </summary>
+        /// <returns>The argument list.</returns>
+        public IReadOnlyList<Constant> Arguments { get; private set; }
+
+        /// <summary>
+        /// Gets the type for this attribute.
+        /// </summary>
+        /// <returns>The attribute type.</returns>
+        public IType AttributeType { get; private set; }
+
+        private static ConcurrentDictionary<string, IType> attrTypes = new ConcurrentDictionary<string, IType>();
+
+        private static IType SynthesizeAttributeType(string name)
+        {
+            return new DescribedType(new SimpleName(name).Qualify(), null);
+        }
+    }
+}
