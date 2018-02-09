afc8e0d7e25d1e9e905f9da32b1174d877a4b058
jonathanvdc <jonathan.vdc@outlook.com>
2018-02-09 14:25:56 +0100

    Define Constant, BooleanConstant

---
+++ Flame.Compiler/Constant.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace Flame.Compiler
+{
+    /// <summary>
+    /// A constant value.
+    /// </summary>
+    public abstract class Constant : IEquatable<Constant>
+    {
+        /// <summary>
+        /// Tests if this constant is equal to another constant.
+        /// </summary>
+        /// <param name="other">The other constant.</param>
+        /// <returns>
+        /// <c>true</c> if the constants are equal; otherwise, <c>false</c>.
+        /// </returns>
+        public abstract bool Equals(Constant other);
+
+        /// <inheritdoc/>
+        public override bool Equals(object obj)
+        {
+            return obj is Constant && Equals((Constant)obj);
+        }
+
+        /// <inheritdoc/>
+        public abstract override int GetHashCode();
+
+        /// <summary>
+        /// Tests if two constants are equal.
+        /// </summary>
+        /// <param name="left">A first constant.</param>
+        /// <param name="right">A second constant.</param>
+        /// <returns>
+        /// <c>true</c> if the constants are equal; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool operator==(Constant left, Constant right)
+        {
+            return object.Equals(left, right);
+        }
+
+        /// <summary>
+        /// Tests if two constants are not equal.
+        /// </summary>
+        /// <param name="left">A first constant.</param>
+        /// <param name="right">A second constant.</param>
+        /// <returns>
+        /// <c>false</c> if the constants are equal; otherwise, <c>true</c>.
+        /// </returns>
+        public static bool operator!=(Constant left, Constant right)
+        {
+            return !object.Equals(left, right);
+        }
+    }
+}
---
+++ Flame.Compiler/Constants/BooleanConstant.cs
@@ -0,0 +1,36 @@
+﻿namespace Flame.Compiler.Constants
+{
+    /// <summary>
+    /// A Boolean constant.
+    /// </summary>
+    public sealed class BooleanConstant : Constant
+    {
+        /// <summary>
+        /// Creates a constant from a value.
+        /// </summary>
+        /// <param name="value"></param>
+        public BooleanConstant(bool value)
+        {
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Gets the value represented by this constant.
+        /// </summary>
+        /// <returns>The constant value.</returns>
+        public bool Value { get; private set; }
+
+        /// <inheritdoc/>
+        public override bool Equals(Constant other)
+        {
+            return other is BooleanConstant
+                && Value == ((BooleanConstant)other).Value;
+        }
+
+        /// <inheritdoc/>
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }
+    }
+}
