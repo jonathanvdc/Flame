9030854dce5e52fe10df1bdd7f94426eb4603a71
jonathanvdc <jonathan.vdc@outlook.com>
2018-06-06 19:37:08 +0200

    Create a basic method resolution test

--- UnitTests/Flame.Clr/MemberResolutionTests.cs
+++ UnitTests/Flame.Clr/MemberResolutionTests.cs
@@ -17,6 +17,9 @@ namespace UnitTests.Flame.Clr
             Mono.Cecil.ModuleDefinition.ReadModule(typeof(object).Module.FullyQualifiedName).Assembly,
             NullAssemblyResolver.Instance);
 
+        private const string StringBoxName = "System.String box*";
+        private const string Int32Name = "System.Int32";
+
         [Test]
         public void ResolveStringEmpty()
         {
@@ -27,7 +30,7 @@ namespace UnitTests.Flame.Clr
             Assert.IsNotNull(emptyField);
             Assert.AreEqual(emptyField.Name.ToString(), emptyFieldRef.Name);
             Assert.IsTrue(emptyField.IsStatic);
-            Assert.AreEqual(emptyField.FieldType.FullName.ToString(), "System.String box*");
+            Assert.AreEqual(emptyField.FieldType.FullName.ToString(), StringBoxName);
         }
 
         [Test]
@@ -40,7 +43,45 @@ namespace UnitTests.Flame.Clr
             Assert.IsNotNull(minValue);
             Assert.AreEqual(minValue.Name.ToString(), minValueRef.Name);
             Assert.IsTrue(minValue.IsStatic);
-            Assert.AreEqual(minValue.FieldType.FullName.ToString(), "System.Int32");
+            Assert.AreEqual(minValue.FieldType.FullName.ToString(), Int32Name);
         }
+
+        [Test]
+        public void ResolveInt32Parse()
+        {
+            var ts = mscorlib.Definition.MainModule.TypeSystem;
+            var parseRef = ts.Int32
+                .Resolve()
+                .Methods
+                .Single(m => m.Name == "Parse" && m.Parameters.Count == 1);
+
+            var parse = mscorlib.Resolve(parseRef);
+            Assert.IsNotNull(parse);
+            Assert.AreEqual(parse.Name.ToString(), parseRef.Name);
+            Assert.IsTrue(parse.IsStatic);
+            Assert.AreEqual(parse.ReturnParameter.Type.FullName.ToString(), Int32Name);
+            Assert.AreEqual(parse.GenericParameters.Count, 0);
+            Assert.AreEqual(parse.Parameters.Count, 1);
+            Assert.AreEqual(parse.Parameters[0].Type.FullName.ToString(), StringBoxName);
+        }
+
+        // [Test]
+        // public void ResolveStringIsNullOrEmpty()
+        // {
+        //     var ts = mscorlib.Definition.MainModule.TypeSystem;
+        //     var isNullOrEmptyRef = ts.String
+        //         .Resolve()
+        //         .Methods
+        //         .Single(m => m.Name == "IsNullOrEmpty");
+
+        //     var isNullOrEmpty = mscorlib.Resolve(isNullOrEmptyRef);
+        //     Assert.IsNotNull(isNullOrEmpty);
+        //     Assert.AreEqual(isNullOrEmpty.Name.ToString(), isNullOrEmpty.Name);
+        //     Assert.IsTrue(isNullOrEmpty.IsStatic);
+        //     Assert.AreEqual(isNullOrEmpty.ReturnParameter.Type.FullName.ToString(), StringBoxName);
+        //     Assert.AreEqual(isNullOrEmpty.GenericParameters.Count, 0);
+        //     Assert.AreEqual(isNullOrEmpty.Parameters.Count, 1);
+        //     Assert.AreEqual(isNullOrEmpty.Parameters[0].Type.FullName.ToString(), StringBoxName);
+        // }
     }
 }
