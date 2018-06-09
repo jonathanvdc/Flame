b1ec25110d208d618f9f6252a23aadcebdb43135
jonathanvdc <jonathan.vdc@outlook.com>
2018-06-09 19:08:27 +0200

    Introduce MutableTypeEnvironment class

--- Flame/TypeSystem/TypeEnvironment.cs
+++ Flame/TypeSystem/TypeEnvironment.cs
@@ -30,4 +30,47 @@ namespace Flame.TypeSystem
             int rank,
             out IType arrayType);
     }
+
+    /// <summary>
+    /// A type environment that wraps an inner type environment that
+    /// can be changed at will.
+    ///
+    /// The main use-case for this kind of environment is a situation
+    /// where the type environment for an assembly is defined by that
+    /// assembly itself but the assembly does not allow for the type
+    /// environment to change.
+    /// </summary>
+    public sealed class MutableTypeEnvironment : TypeEnvironment
+    {
+        /// <summary>
+        /// Creates a mutable type environment.
+        /// </summary>
+        /// <param name="innerEnvironment">
+        /// An inner environment to forward requests to.
+        /// </param>
+        public MutableTypeEnvironment(
+            TypeEnvironment innerEnvironment)
+        {
+            this.InnerEnvironment = innerEnvironment;
+        }
+
+        /// <summary>
+        /// Gets or sets the inner environment, to which all requests
+        /// are forwarded by this type environment.
+        /// </summary>
+        /// <returns>The inner type environment.</returns>
+        public TypeEnvironment InnerEnvironment { get; set; }
+
+        /// <inheritdoc/>
+        public override bool TryMakeArrayType(
+            IType elementType,
+            int rank,
+            out IType arrayType)
+        {
+            return InnerEnvironment.TryMakeArrayType(
+                elementType,
+                rank,
+                out arrayType);
+        }
+    }
 }
