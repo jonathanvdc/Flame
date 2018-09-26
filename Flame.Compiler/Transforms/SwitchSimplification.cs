a1bcea1e10cfa64a09870051c52b1519b2bebe4f
jonathanvdc <jonathan.vdc@outlook.com>
2018-09-26 09:23:13 +0200

    Teach switch simplification to fold integer conversions into switches

--- Flame.Compiler/Transforms/SwitchSimplification.cs
+++ Flame.Compiler/Transforms/SwitchSimplification.cs
@@ -1,6 +1,11 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 using Flame.Compiler.Flow;
 using Flame.Compiler.Instructions;
+using Flame.Constants;
+using Flame.TypeSystem;
 
 namespace Flame.Compiler.Transforms
 {
@@ -32,25 +37,82 @@ namespace Flame.Compiler.Transforms
                 if (block.Flow is SwitchFlow)
                 {
                     var flow = (SwitchFlow)block.Flow;
-                    var value = SimplifyInstruction(flow.SwitchValue, graphBuilder);
-                    if (value.Prototype is ConstantPrototype)
+                    var simpleFlow = SimplifySwitchFlow(flow, graphBuilder);
+                    if (flow != simpleFlow)
                     {
-                        // Turn the switch into a jump.
-                        var constant = ((ConstantPrototype)value.Prototype).Value;
-                        var valuesToBranches = flow.ValueToBranchMap;
-                        block.Flow = new JumpFlow(
-                            valuesToBranches.ContainsKey(constant)
-                            ? valuesToBranches[constant]
-                            : flow.DefaultBranch);
-                    }
-                    else if (ArithmeticIntrinsics.IsArithmeticIntrinsicPrototype(value.Prototype))
-                    {
-                        var proto = (IntrinsicPrototype)value.Prototype;
-                        throw new NotImplementedException();
+                        block.Flow = simpleFlow;
                     }
                 }
             }
             return graphBuilder.ToImmutable();
+        }
+
+        private static BlockFlow SimplifySwitchFlow(SwitchFlow flow, FlowGraphBuilder graph)
+        {
+            var value = SimplifyInstruction(flow.SwitchValue, graph);
+            if (value.Prototype is ConstantPrototype)
+            {
+                // Turn the switch into a jump.
+                var constant = ((ConstantPrototype)value.Prototype).Value;
+                var valuesToBranches = flow.ValueToBranchMap;
+                return new JumpFlow(
+                    valuesToBranches.ContainsKey(constant)
+                    ? valuesToBranches[constant]
+                    : flow.DefaultBranch);
+            }
+            else if (ArithmeticIntrinsics.IsArithmeticIntrinsicPrototype(value.Prototype))
+            {
+                var proto = (IntrinsicPrototype)value.Prototype;
+                var intrinsicName = ArithmeticIntrinsics.ParseArithmeticIntrinsicName(proto.Name);
+                if (intrinsicName == ArithmeticIntrinsics.Operators.Convert
+                    && proto.ParameterCount == 1
+                    && flow.IsIntegerSwitch)
+                {
+                    // We can eliminate instructions that extend integers
+                    // by changing the values in the list of cases.
+                    var operand = proto.GetArgumentList(value).Single();
+                    var operandType = graph.GetValueType(operand);
+                    var convType = proto.ResultType;
+                    var operandSpec = operandType.GetIntegerSpecOrNull();
+                    var convSpec = convType.GetIntegerSpecOrNull();
+
+                    if (operandSpec.Size > convSpec.Size)
+                    {
+                        // We can't handle this case. To handle it anyway
+                        // would require us to introduce additional cases
+                        // and that's costly.
+                        return flow;
+                    }
+
+                    var caseList = new List<SwitchCase>();
+                    foreach (var switchCase in flow.Cases)
+                    {
+                        // Retain only those switch cases that have values
+                        // that are in the range of the conversion function.
+                        var values = ImmutableHashSet.CreateBuilder<Constant>();
+                        foreach (var val in switchCase.Values.Cast<IntegerConstant>())
+                        {
+                            if (val.Cast(operandSpec).Cast(convSpec).Equals(val))
+                            {
+                                values.Add(val);
+                            }
+                        }
+                        if (values.Count > 0)
+                        {
+                            caseList.Add(new SwitchCase(values.ToImmutableHashSet(), switchCase.Branch));
+                        }
+                    }
+                    return SimplifySwitchFlow(
+                        new SwitchFlow(
+                            Instruction.CreateCopy(
+                                operandType,
+                                operand),
+                            caseList,
+                            flow.DefaultBranch),
+                        graph);
+                }
+            }
+            return flow;
         }
 
         /// <summary>
