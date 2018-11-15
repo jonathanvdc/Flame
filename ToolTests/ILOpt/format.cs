fecc905564cc8a1231c9a292ad4daa8070c32402
jonathanvdc <jonathan.vdc@outlook.com>
2018-11-15 16:05:38 +0100

    Add another ilopt test

---
+++ ToolTests/ILOpt/format.cs
@@ -0,0 +1,14 @@
+//! run
+
+using System;
+
+public static class Program
+{
+    public static void Main()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"i = {i}");
+        }
+    }
+}
