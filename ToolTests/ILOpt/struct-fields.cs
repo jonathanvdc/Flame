630bc2e34496f76dac158f84158534c05a29729c
jonathanvdc <jonathan.vdc@outlook.com>
2018-12-13 11:44:23 +0100

    Add a ldfld/stfld test

---
+++ ToolTests/ILOpt/struct-fields.cs
@@ -0,0 +1,16 @@
+using System;
+
+public struct StringContainer
+{
+    public string Contents;
+}
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var container = new StringContainer();
+        container.Contents = args[0];
+        Console.WriteLine(container.Contents);
+    }
+}
