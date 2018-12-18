b68aaf5b5397a6062d16b5b8373b2f4b39760474
jonathanvdc <jonathan.vdc@outlook.com>
2018-12-18 12:43:48 +0100

    Add a struct newobj test

---
+++ ToolTests/ILOpt/struct-newobj.cs
@@ -0,0 +1,21 @@
+using System;
+
+public struct StringContainer
+{
+    public StringContainer(string contents)
+    {
+        this.Contents = contents;
+    }
+
+    public string Contents;
+}
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var containers = new StringContainer[1];
+        containers[0] = new StringContainer(args[0]);
+        Console.WriteLine(containers[0].Contents);
+    }
+}
