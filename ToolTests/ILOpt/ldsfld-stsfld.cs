16483a3502de76c6ae72b12dcab8d2f3eb7e6f24
jonathanvdc <jonathan.vdc@outlook.com>
2018-12-19 12:06:58 +0100

    Add a ldsfld, stsfld test

---
+++ ToolTests/ILOpt/ldsfld-stsfld.cs
@@ -0,0 +1,12 @@
+using System;
+
+public static class Program
+{
+    public static string globalValue;
+
+    public static void Main(string[] args)
+    {
+        globalValue = args[0];
+        Console.WriteLine(globalValue);
+    }
+}
