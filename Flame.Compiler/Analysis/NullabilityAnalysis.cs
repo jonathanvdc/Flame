098c3b7110d645d39bf7a4957f4b2266c6ae5541
jonathanvdc <jonathan.vdc@outlook.com>
2019-01-11 19:12:29 +0100

    Make nullability analysis more versatile

--- Flame.Compiler/Analysis/NullabilityAnalysis.cs
+++ Flame.Compiler/Analysis/NullabilityAnalysis.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using Flame.Compiler.Instructions;
+using Flame.TypeSystem;
 
 namespace Flame.Compiler.Analysis
 {
@@ -49,6 +51,165 @@ namespace Flame.Compiler.Analysis
         public bool IsDereferenceable(ValueTag value)
         {
             return IsNonNull(value) && IsDereferenceableOrNull(value);
+        }
+
+        /// <summary>
+        /// Tells if a particular instruction always produces a non-<c>null</c> pointer.
+        /// </summary>
+        /// <param name="instruction">
+        /// An instruction to test for nullability.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if <paramref name="instruction"/> will always produce a
+        /// non-<c>null</c> pointer; otherwise, <c>false</c>.
+        /// </returns>
+        public virtual bool IsNonNull(Instruction instruction)
+        {
+            if (IsNonNull(instruction.ResultType))
+            {
+                // Maybe the instruction's result type is intrinsically
+                // non-nullable. In that case, we don't even need to look
+                // at the instruction itself.
+                return true;
+            }
+
+            // Okay, so the instruction's type is not nullable. But
+            // maybe we can know for sure that the instruction's result
+            // is not nullable by looking at the instruction's prototype.
+            var proto = instruction.Prototype;
+            if (IsNonNull(proto))
+            {
+                return true;
+            }
+
+            // At this point, it turns out that neither the instruction's
+            // result type nor its prototype guarantee non-nullability.
+            //
+            // One last thing that we can try is to do is infer non-nullability
+            // from the instruction's arguments for particular prototypes, like
+            // reinterpret casts.
+            if (proto is ReinterpretCastPrototype
+                || proto is CopyPrototype)
+            {
+                return IsNonNull(instruction.Arguments[0]);
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Tells if a particular instruction always produces a
+        /// pointer that is either dereferenceable or <c>null</c>.
+        /// </summary>
+        /// <param name="instruction">
+        /// An instruction to test for dereferenceability.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if <paramref name="instruction"/> may produce a <c>null</c>
+        /// pointer; otherwise, <c>false</c>.
+        /// </returns>
+        public virtual bool IsDereferenceableOrNull(Instruction instruction)
+        {
+            if (IsDereferenceableOrNull(instruction.ResultType))
+            {
+                // Maybe the instruction's result type is intrinsically
+                // dereferenceable or null. In that case, we don't even
+                // need to look at the instruction itself.
+                return true;
+            }
+
+            // Okay, so the instruction's type is not necessarily
+            // dereferenceable or null. But maybe we can know for
+            // sure that the instruction's result is dereferenceable
+            // or null by looking at the instruction's prototype.
+            var proto = instruction.Prototype;
+            if (IsDereferenceableOrNull(proto))
+            {
+                return true;
+            }
+
+            // At this point, it turns out that neither the instruction's
+            // result type nor its prototype guarantee dereferenceability.
+            //
+            // One last thing that we can try is to do is infer dereferenceability
+            // from the instruction's arguments for particular prototypes, like
+            // copies.
+            if (proto is CopyPrototype
+                && IsNonNull(instruction.Arguments[0]))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Tests if a type is never nullable.
+        /// </summary>
+        /// <param name="type">
+        /// The type to test for non-nullability.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if a <c>null</c> pointer is an instance of
+        /// <paramref name="type"/>; otherwise, <c>false</c>.
+        /// </returns>
+        public virtual bool IsNonNull(IType type)
+        {
+            var pointerType = type as PointerType;
+            return (pointerType == null && !(type is IGenericParameter))
+                || pointerType.Kind == PointerKind.Reference;
+        }
+
+        /// <summary>
+        /// Tests if a type is always either dereferenceable or <c>null</c>.
+        /// </summary>
+        /// <param name="type">
+        /// The type to test for dereferenceability.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> all instances of <paramref name="type"/> are either
+        /// dereferenceable or <c>null</c>; otherwise, <c>false</c>.
+        /// </returns>
+        public virtual bool IsDereferenceableOrNull(IType type)
+        {
+            var pointerType = type as PointerType;
+            return (pointerType == null && !(type is IGenericParameter))
+                || pointerType.Kind == PointerKind.Box
+                || pointerType.Kind == PointerKind.Reference;
+        }
+
+        internal static bool IsNonNull(InstructionPrototype prototype)
+        {
+            return IsDereferenceable(prototype);
+        }
+
+        internal static bool IsDereferenceableOrNull(InstructionPrototype prototype)
+        {
+            return IsDereferenceable(prototype);
+        }
+
+        private static bool IsDereferenceable(InstructionPrototype prototype)
+        {
+            // * Allocas always produce non-null pointers that
+            //   are dereferenceable.
+            //
+            // * Unbox instructions that succeed always produce
+            //   non-null pointers that are dereferenceable. If
+            //   they don't succeed, then an exception is thrown
+            //   and the result can't be used.
+            //
+            // * Get-field-pointer instructions that succeed
+            //   always produce non-null pointers that are
+            //   dereferenceable. If they don't succeed, then
+            //   an exception is thrown and the result can't be used.
+            //
+            // * Ditto for get-static-field-pointer instructions.
+
+            return prototype is AllocaPrototype
+                || prototype is UnboxPrototype
+                || prototype is GetFieldPointerPrototype
+                || prototype is GetStaticFieldPointerPrototype
+                || prototype is NewObjectPrototype;
         }
     }
 
@@ -123,31 +284,18 @@ namespace Flame.Compiler.Analysis
             foreach (var instruction in graph.Instructions)
             {
                 var proto = instruction.Instruction.Prototype;
-                if (proto is AllocaPrototype
-                    || proto is UnboxPrototype
-                    || proto is GetFieldPointerPrototype
-                    || proto is GetStaticFieldPointerPrototype)
+                var tag = instruction.Tag;
+                if (!ValueNullability.IsNonNull(proto))
                 {
-                    // * Allocas always produce non-null pointers that
-                    //   are dereferenceable.
-                    //
-                    // * Unbox instructions that succeed always produce
-                    //   non-null pointers that are dereferenceable. If
-                    //   they don't succeed, then an exception is thrown
-                    //   and the result can't be used.
-                    //
-                    // * Get-field-pointer instructions that succeed
-                    //   always produce non-null pointers that are
-                    //   dereferenceable. If they don't succeed, then
-                    //   an exception is thrown and the result can't be used.
-                    continue;
+                    nullable.Add(tag);
                 }
-                else
+
+                if (!ValueNullability.IsDereferenceableOrNull(proto))
                 {
-                    nullable.Add(instruction);
-                    nonderef.Add(instruction);   
+                    nonderef.Add(tag);
                 }
             }
+
             return new ExplicitValueNullability(
                 nullable.ToImmutable(),
                 nonderef.ToImmutable());
