2bf9f38a49838be05ce4a24d2fbdf1c3fe78de56
jonathanvdc <jonathan.vdc@outlook.com>
2019-02-16 20:55:39 +0100

    Define an equality comparer that first applies a function

---
+++ Flame/Collections/MappedComparer.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+
+namespace Flame.Collections
+{
+    /// <summary>
+    /// An equality comparer that applies a mapping to elements
+    /// before comparing them.
+    /// </summary>
+    /// <typeparam name="T1">The argument type of the mapping.</typeparam>
+    /// <typeparam name="T2">The return type of the mapping.</typeparam>
+    public sealed class MappedComparer<T1, T2> : IEqualityComparer<T1>
+    {
+        /// <summary>
+        /// Creates an equality comparer that first applies a mapping
+        /// and then compares the results using the default comparer.
+        /// </summary>
+        /// <param name="transform">
+        /// A mapping that takes an element and transforms it before it is compared.
+        /// </param>
+        public MappedComparer(Func<T1, T2> transform)
+            : this(transform, EqualityComparer<T2>.Default)
+        { }
+
+        /// <summary>
+        /// Creates an equality comparer that first applies a mapping
+        /// and then compares the results using a custom comparer.
+        /// </summary>
+        /// <param name="transform">
+        /// A mapping that takes an element and transforms it before it is compared.
+        /// </param>
+        /// <param name="resultComparer">
+        /// A comparer for the results of <paramref name="transform"/>.
+        /// </param>
+        public MappedComparer(Func<T1, T2> transform, IEqualityComparer<T2> resultComparer)
+        {
+            this.Transform = transform;
+            this.ResultComparer = resultComparer;
+        }
+
+        /// <summary>
+        /// Takes a value of type <typeparamref name="T1"/> and maps it
+        /// to a value of type <typeparamref name="T2"/>.
+        /// </summary>
+        public readonly Func<T1, T2> Transform;
+
+        /// <summary>
+        /// An equality comparer for values of type <typeparamref name="T2"/>.
+        /// </summary>
+        public readonly IEqualityComparer<T2> ResultComparer;
+
+        public bool Equals(T1 x, T1 y)
+        {
+            return ResultComparer.Equals(Transform(x), Transform(y));
+        }
+
+        public int GetHashCode(T1 obj)
+        {
+            return ResultComparer.GetHashCode(Transform(obj));
+        }
+    }
+}
