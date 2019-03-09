b4e9d8bab2bc164e867df986b6d9f269e8bf9b94
jonathanvdc <jonathan.vdc@outlook.com>
2019-03-09 16:24:47 +0100

    Add another value type 'newobj' test

---
+++ ToolTests/ILOpt/struct-newobj2.cs
@@ -0,0 +1,29 @@
+//! run:hello there
+
+using System;
+
+public struct StringWrapper
+{
+    public StringWrapper(string value)
+    {
+        this.Value = value;
+    }
+
+    public string Value { get; private set; }
+}
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        foreach (var arg in args)
+        {
+            Print(new StringWrapper(arg));
+        }
+    }
+
+    public static void Print(StringWrapper wrapper)
+    {
+        Console.WriteLine(wrapper.Value);
+    }
+}
