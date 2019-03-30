6e9cef5428eac69c3dc0f2c7b191c9197b233a84
jonathanvdc <jonathan.vdc@outlook.com>
2019-03-30 17:02:49 +0100

    Extend ternary test

--- ToolTests/ILOpt/ternary.cs
+++ ToolTests/ILOpt/ternary.cs
@@ -7,5 +7,6 @@ public static class Program
     public static void Main(string[] args)
     {
         Console.WriteLine("{0}", args.Length == 0 ? "" : args[0]);
+        Console.WriteLine("{0}", args.Length == 1 ? "" : args[0]);
     }
 }
