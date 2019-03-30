772f60b1b00f9d4bbff202ab789ab4ad51f52bb6
jonathanvdc <jonathan.vdc@outlook.com>
2019-03-30 17:28:51 +0100

    Add a LINQ test

---
+++ ToolTests/ILOpt/linq.cs
@@ -0,0 +1,30 @@
+// Test case adapted from MSDN example at
+// https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/linq/introduction-to-linq-queries
+
+//! run
+
+using System;
+using System.Linq;
+
+class IntroToLINQ
+{
+    static void Main()
+    {
+        // The Three Parts of a LINQ Query:
+        // 1. Data source.
+        int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+
+        // 2. Query creation.
+        // numQuery is an IEnumerable<int>
+        var numQuery =
+            from num in numbers
+            where (num % 2) == 0
+            select num;
+
+        // 3. Query execution.
+        foreach (int num in numQuery)
+        {
+            Console.Write("{0,1} ", num);
+        }
+    }
+}
