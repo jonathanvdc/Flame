205a6cb7b7448f5a3fc41dcf8afc84d7b70fb43b
jonathanvdc <jonathan.vdc@outlook.com>
2019-03-30 16:50:04 +0100

    Add a test that branches on a natural uint

---
+++ ToolTests/ILOpt/ternary.cs
@@ -0,0 +1,11 @@
+//! run:hello there
+
+using System;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("{0}", args.Length == 0 ? "" : args[0]);
+    }
+}
