c041b86d941a75cf663f8eebf41c0c1f576580e5
jonathanvdc <jonathan.vdc@outlook.com>
2019-04-08 18:23:20 +0200

    Add a 'select' LINQ query test

---
+++ ToolTests/ILOpt/linq-select.cs
@@ -0,0 +1,24 @@
+//! run
+
+using System;
+using System.Linq;
+
+class IntroToLINQ
+{
+    static void Main()
+    {
+        // Define a sequence of numbers.
+        int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+
+        // Compute the square of those numbers
+        var numQuery =
+            from num in numbers
+            select num * num;
+
+        // Print the squared numbers.
+        foreach (int num in numQuery)
+        {
+            Console.Write("{0,1} ", num);
+        }
+    }
+}
