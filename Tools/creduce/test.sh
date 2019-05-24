b9d407343c2a9169623483e7a84b8ccf09dd272a
jonathanvdc <jonathan.vdc@outlook.com>
2019-05-24 20:02:58 +0200

    Tweak creduce interestingness test script

--- Tools/creduce/test.sh
+++ Tools/creduce/test.sh
@@ -9,7 +9,7 @@
 #
 
 csc /o+ main.cs && \
+mono main.exe > normal-output.txt && \
 $(dirname $0)/../../ILOpt/bin/Release/ilopt.exe main.exe && \
 (! mono main.opt.exe > opt-output.txt || \
- ! mono main.exe > normal-output.txt || \
  ! diff normal-output.txt opt-output.txt)
