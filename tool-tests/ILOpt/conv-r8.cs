5a9ac0c5ead4e4067933e217f645db0fab3e2c49
jonathanvdc <jonathan.vdc@outlook.com>
2019-05-25 12:50:31 +0200

    Add a conv.r8 test

---
+++ tool-tests/ILOpt/conv-r8.cs
@@ -0,0 +1,15 @@
+//! run
+
+using System;
+
+public static class Program
+{
+    private static int one = 1;
+    private static int lots = 2048;
+
+    public static void Main()
+    {
+        Console.WriteLine((double)one);
+        Console.WriteLine((double)lots);
+    }
+}
