9f17937b351a6a1fcfbc448d7ab6853ef539134d
jonathanvdc <jonathan.vdc@outlook.com>
2019-06-12 18:57:07 +0200

    [LLVM] Add a static field test

---
+++ tool-tests/IL2LLVM/static-field-libc.cs
@@ -0,0 +1,44 @@
+//! run
+
+using System.Runtime.InteropServices;
+
+public static unsafe class Program
+{
+    [DllImport("libc.so.6")]
+    private static extern void* malloc(ulong size);
+
+    [DllImport("libc.so.6")]
+    private static extern void free(void* data);
+
+    [DllImport("libc.so.6")]
+    private static extern int puts(byte* str);
+
+    private static byte* format;
+
+    private static void FillString(byte* str)
+    {
+        *str = (byte)'h';
+        *(str + 1) = (byte)'i';
+        *(str + 2) = (byte)'\0';
+    }
+
+    private static void Initialize()
+    {
+        format = (byte*)malloc(3);
+        FillString(format);
+    }
+
+    private static void Deinitialize()
+    {
+        free((void*)format);
+        // format = null;
+    }
+
+    public static int Main()
+    {
+        Initialize();
+        puts(format);
+        Deinitialize();
+        return 0;
+    }
+}
