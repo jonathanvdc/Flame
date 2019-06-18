e527833ad351bb375e18092797cf839179082d1f
jonathanvdc <jonathan.vdc@outlook.com>
2019-06-18 11:04:01 +0200

    [LLVM] Include a dynamic cast example

---
+++ tool-tests/IL2LLVM/dynamic-cast-libc.cs
@@ -0,0 +1,62 @@
+//! run
+
+using System.Runtime.InteropServices;
+
+public unsafe class Base
+{
+    [DllImport("libc.so.6")]
+    public static extern void* malloc(ulong size);
+
+    [DllImport("libc.so.6")]
+    public static extern void free(void* data);
+
+    [DllImport("libc.so.6")]
+    public static extern int puts(byte* str);
+
+    public byte* CreateFormatString()
+    {
+        var derived = this as Derived;
+        if (derived != null)
+        {
+            return derived.CreateFormatStringOverride();
+        }
+        else
+        {
+            var str = (byte*)malloc(3);
+            *str = (byte)'h';
+            *(str + 1) = (byte)'i';
+            *(str + 2) = (byte)'\0';
+            return str;
+        }
+    }
+
+    public void Print()
+    {
+        var str = CreateFormatString();
+        puts(str);
+        free((void*)str);
+    }
+}
+
+public unsafe class Derived : Base
+{
+    public byte* CreateFormatStringOverride()
+    {
+        var str = (byte*)malloc(4);
+        *str = (byte)'b';
+        *(str + 1) = (byte)'y';
+        *(str + 2) = (byte)'e';
+        *(str + 3) = (byte)'\0';
+        return str;
+    }
+}
+
+public static unsafe class Program
+{
+    public static int Main()
+    {
+        new Base().Print();
+        new Derived().Print();
+        return 0;
+    }
+}
