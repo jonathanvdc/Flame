b19a7afc7cec96ad44e0f6007066d025480626bc
jonathanvdc <jonathan.vdc@outlook.com>
2019-08-16 19:00:32 +0200

    [Turbo] Update example program

--- src/Examples/TurboKernels/Program.cs
+++ src/Examples/TurboKernels/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using Turbo;
 
 namespace TurboKernels
@@ -14,10 +15,10 @@ namespace TurboKernels
         {
             Parallel.ForAsync(20, Kernel).Wait();
 
-            // TODO: implement logic that copies changes to call target
-            // (closure over 'x' in this case) back from the device.
+            // Atomically increment a value twenty times on the device.
             int x = 10;
-            Parallel.ForAsync(20, () => { x++; }).Wait();
+            Parallel.ForAsync(20, () => { Interlocked.Add(ref x, 1); }).Wait();
+            Console.WriteLine(x);
         }
     }
 }
