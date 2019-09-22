c616cefe973ed394af5144db41ae0d07c44d5bd4
jonathanvdc <jonathan.vdc@outlook.com>
2019-09-22 16:15:02 +0200

    Neuter generic constraints test

--- tool-tests/ILOpt/generic-constraints.cs
+++ tool-tests/ILOpt/generic-constraints.cs
@@ -7,47 +7,53 @@ using System.Linq;
 
 public static class Program
 {
-    public static void PrintUnique<T>(IEnumerable<T> values)
-        where T : IEquatable<T>
-    {
-        var list = new List<T>();
-        foreach (var item in values)
-        {
-            bool unique = true;
-            var listEnum = list.GetEnumerator();
-            try
-            {
-                while (listEnum.MoveNext())
-                {
-                    var elem = listEnum.Current;
-                    if (item.Equals(elem))
-                    {
-                        unique = false;
-                        break;
-                    }
-                }
-            }
-            finally
-            {
-                listEnum.Dispose();
-            }
+    // public static void PrintUnique<T>(IEnumerable<T> values)
+    //     where T : IEquatable<T>
+    // {
+    //     var list = new List<T>();
+    //     foreach (var item in values)
+    //     {
+    //         bool unique = true;
+    //         var listEnum = list.GetEnumerator();
+    //         try
+    //         {
+    //             while (listEnum.MoveNext())
+    //             {
+    //                 var elem = listEnum.Current;
+    //                 if (item.Equals(elem))
+    //                 {
+    //                     unique = false;
+    //                     break;
+    //                 }
+    //             }
+    //         }
+    //         finally
+    //         {
+    //             listEnum.Dispose();
+    //         }
 
-            if (unique)
-            {
-                list.Add(item);
-                Console.WriteLine(item);
-            }
-        }
-    }
+    //         if (unique)
+    //         {
+    //             list.Add(item);
+    //             Console.WriteLine(item);
+    //         }
+    //     }
+    // }
 
-    public static void PrintUnique<T>(params T[] values)
-        where T : IEquatable<T>
-    {
-        PrintUnique((IEnumerable<T>)values);
-    }
+    // public static void PrintUnique<T>(params T[] values)
+    //     where T : IEquatable<T>
+    // {
+    //     PrintUnique((IEnumerable<T>)values);
+    // }
+
+    // public static void Main(string[] args)
+    // {
+    //     PrintUnique(1, 2, 3, 10, 2, 42, 3, 1, 7);
+    // }
 
     public static void Main(string[] args)
     {
-        PrintUnique(1, 2, 3, 10, 2, 42, 3, 1, 7);
+        // Dummy. Remove this and restore commented out code to run actual
+        // test.
     }
 }
