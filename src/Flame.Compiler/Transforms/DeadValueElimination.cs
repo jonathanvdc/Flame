c57d6851c105f9801954b39899cbb0748c50cd3a
jonathanvdc <jonathan.vdc@outlook.com>
2019-09-23 23:10:25 +0200

    Remove dead allocas in dead value elimination

--- src/Flame.Compiler/Transforms/DeadValueElimination.cs
+++ src/Flame.Compiler/Transforms/DeadValueElimination.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using Flame.Compiler.Analysis;
+using Flame.Compiler.Instructions;
 
 namespace Flame.Compiler.Transforms
 {
@@ -28,7 +30,7 @@ namespace Flame.Compiler.Transforms
         public override FlowGraph Apply(FlowGraph graph)
         {
             // This transform simply builds a set of live values and
-            // then delete everything that's not.
+            // then deletes everything that's not.
 
             // Compute the live values.
             var liveValues = ComputeLiveValues(graph);
@@ -52,6 +54,14 @@ namespace Flame.Compiler.Transforms
 
             // Effectful instructions are always live.
             liveValues.UnionWith(graph.GetAnalysisResult<EffectfulInstructions>().Instructions);
+
+            // Remove stores to local variables from the set of
+            // effectful instructions.
+            var localStores = GetLocalStores(graph);
+            foreach (var set in localStores.Values)
+            {
+                liveValues.ExceptWith(set);
+            }
 
             // Entry point parameters are always live too.
             liveValues.UnionWith(graph.GetBasicBlock(graph.EntryPointTag).ParameterTags);
@@ -109,11 +119,92 @@ namespace Flame.Compiler.Transforms
                         {
                             worklist.Enqueue(arg);
                         }
+                        HashSet<ValueTag> storeDependencies;
+                        if (localStores.TryGetValue(value, out storeDependencies))
+                        {
+                            foreach (var dep in storeDependencies)
+                            {
+                                worklist.Enqueue(dep);
+                            }
+                        }
                     }
                 }
             }
 
             return liveValues;
+        }
+
+        /// <summary>
+        /// Creates a mapping of instructions that point to local variables to the
+        /// stores that operate on those local variables.
+        /// </summary>
+        /// <param name="graph">A control flow graph.</param>
+        private static Dictionary<ValueTag, HashSet<ValueTag>> GetLocalStores(FlowGraph graph)
+        {
+            var results = new Dictionary<ValueTag, HashSet<ValueTag>>();
+            foreach (var instruction in graph.NamedInstructions)
+            {
+                if (instruction.Prototype is StorePrototype)
+                {
+                    var pointer = ((StorePrototype)instruction.Prototype).GetPointer(instruction.Instruction);
+                    IEnumerable<ValueTag> localInstructions;
+                    if (TryRecognizeAsLocal(pointer, graph, out localInstructions))
+                    {
+                        foreach (var item in localInstructions)
+                        {
+                            HashSet<ValueTag> localDeps;
+                            if (!results.TryGetValue(item, out localDeps))
+                            {
+                                results[item] = localDeps = new HashSet<ValueTag>();
+                            }
+                            localDeps.Add(instruction);
+                        }
+                    }
+                }
+            }
+            return results;
+        }
+
+        /// <summary>
+        /// Tries to recognize a value as a pointer to an alloca.
+        /// </summary>
+        /// <param name="pointer">A value to inspect.</param>
+        /// <param name="graph">A control flow graph that defines <paramref name="pointer"/>.</param>
+        /// <param name="localInstructions">
+        /// A chain of pointers derived from a local.
+        /// The chain starts at <paramref name="pointer"/> and ends at an alloca.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if <paramref name="pointer"/> is recognized as an alloca or a pointer
+        /// derived from an alloca; otherwise, <c>false</c>.
+        /// </returns>
+        private static bool TryRecognizeAsLocal(
+            ValueTag pointer,
+            FlowGraph graph,
+            out IEnumerable<ValueTag> localInstructions)
+        {
+            NamedInstruction instruction;
+            if (graph.TryGetInstruction(pointer, out instruction))
+            {
+                if (instruction.Prototype is AllocaPrototype
+                    || instruction.Prototype is AllocaArrayPrototype)
+                {
+                    localInstructions = new[] { pointer };
+                    return true;
+                }
+                else if (instruction.Prototype is CopyPrototype
+                    || instruction.Prototype is ReinterpretCastPrototype
+                    || instruction.Prototype is GetFieldPointerPrototype)
+                {
+                    if (TryRecognizeAsLocal(instruction.Arguments[0], graph, out localInstructions))
+                    {
+                        localInstructions = new[] { pointer }.Concat(localInstructions);
+                        return true;
+                    }
+                }
+            }
+            localInstructions = null;
+            return false;
         }
     }
 }
